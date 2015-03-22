75864ba82294841f3d5727419888d2d5102f8d38
Mario <seelim@student.ethz.ch>
2015-03-22 00:42:29 -0400

    added ability to not pass a run-number argument and go through all runs or the first n of them

--- Analyze.py
+++ Analyze.py
@@ -13,9 +13,6 @@ if __name__ == "__main__":
     #initialize_ROOT()
     # get run numbers to analyze from arguments (type: list):
     run_numbers = map(int, [i for i in sys.argv if i.isdigit() == True])
-    assert (len(run_numbers) > 0), "No run specified. Please pass at least one run number as argument."
-
-    print run_numbers
 
     # config
     show_plots = True
@@ -23,26 +20,35 @@ if __name__ == "__main__":
     minimum_statistics = 5 # don't draw bins which contain less than minimum_statistics hits
     run = Run()
 
-    run_numbers =  RunInfo.runs.keys()
+    MaxNrOfRuns = 9999
+    if(len(run_numbers) == 0):
+        MaxNrOfRuns = int(raw_input("No runs specified. Set Maximal number of runs to analyze (0-999) press Enter to select all Runs: "))
+        assert (0 <= MaxNrOfRuns < 1000), "Invalid maximal run quantity"
+        all_run_numbers =  RunInfo.runs.keys()
+        run_numbers = []
+        for i in all_run_numbers:
+            run.SetRun(i)
+            if run.current_run['data_type'] == 0: # 0 = data, 1 = pedestrial, 2 = voltagescan, 3 =  long run, 4 = other
+                run_numbers += [i]
+    print "Starting analysis with ",len(run_numbers[:MaxNrOfRuns])," runs:"
+    print run_numbers[:MaxNrOfRuns]
+
 
     collection = AnalysisCollectionClass()
-    for run_number in run_numbers[:20]:
+    for run_number in run_numbers[:MaxNrOfRuns]:
 
         run.SetRun(run_number)
 
-        if run.current_run['data_type'] == 0: # 0 = data, 1 = pedestrial, 2 = voltagescan, 3 =  long run, 4 = other
+        print run_number
 
-            print run_number
-
-
-            new2DHistConfig = Pad2DHistConfig(50,-0.2,0.15,50,0.03,0.4)
-            #
-            newAnalysis = Analysis(run,new2DHistConfig)
-            newAnalysis.DoAnalysis(minimum_statistics)
-            #newAnalysis.CreatePlots(True,'2D_Signal_dist')
-            # newAnalysis.CreateMeanSignalHistogram(True)
-            newAnalysis.CreateBoth(True)
-            collection.AddAnalysis(newAnalysis)
+        new2DHistConfig = Pad2DHistConfig(50,-0.2,0.15,50,0.03,0.4)
+        #
+        newAnalysis = Analysis(run,new2DHistConfig)
+        newAnalysis.DoAnalysis(minimum_statistics)
+        #newAnalysis.CreatePlots(True,'2D_Signal_dist')
+        # newAnalysis.CreateMeanSignalHistogram(True)
+        newAnalysis.CreateBoth(True)
+        collection.AddAnalysis(newAnalysis)
 
 
     collection.CreateFWHMPlot()
