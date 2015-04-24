8233fcf9092ab31a18b3d0c45e3ea22e6c53176c
Mario <seelim@student.ethz.ch>
2015-04-24 01:49:01 -0400

    DoGhostScan added nut not yet programmed out

--- AbstractClasses/MCPerformance.py
+++ AbstractClasses/MCPerformance.py
@@ -12,7 +12,6 @@ from array import array
 from datetime import datetime
 '''
 To improve:
-    fix infofile (gets empty file)
     import config file
     write config file for later imports
     prohibit memory overfill
@@ -21,14 +20,17 @@ To improve:
 
 class MCPerformance(Elementary):
 
+    def __init__(self, verbose = False):
+        Elementary.__init__(self, verbose=verbose)
+        # Settings:
+        self.tries = 20 # number of repetitions for a certain signal height
+        self.min_percent = 5   # Quantiles
+        self.max_percent = 99  # Quantiles
+
     def DoSignalHeightScan(self, heights=None, hits_per_height=300000):
         gc.disable()
         starttime = datetime.today()
 
-        # Settings:
-        tries = 40 # number of repetitions for a certain signal height
-        min_percent = 5   # Quantiles
-        max_percent = 99  # Quantiles
 
         # ROOT Logfile:
         path = "MC/Performance_Results/"+str(starttime)
@@ -63,19 +65,19 @@ class MCPerformance(Elementary):
         # infofile:
         infofile = open(path+"/info.txt", "w")
         infofile.write("DoSignalHeightScan\n\n")
-        infofile.write("Number of Repetitions for each Amplitude: "+str(tries)+"\n")
+        infofile.write("Number of Repetitions for each Amplitude: "+str(self.tries)+"\n")
         infofile.write("Number of different Amplitudes:           "+str(len(heights))+"\n")
         infofile.write("Hits per Amplitude:                       "+str(hits_per_height)+"\n")
 
         success_prob = []
         ghost_prob = []
         cycle_nr = 0
-        cycles = tries*len(heights)
+        cycles = self.tries*len(heights)
         for height in heights: # add more statistics for each height, not just one try..
             fails = 0
             tot_ghosts = 0
             peaks_generated = 0
-            for repetition in xrange(tries):
+            for repetition in xrange(self.tries):
                 cycle_nr += 1
                 print "\n{0}th repetition with Signal height set to: {1}\n".format(repetition, height)
                 run_object = MCRun(validate=False,verbose=self.verbose,run_number=364)
@@ -101,7 +103,7 @@ class MCPerformance(Elementary):
                     rec_sa_minmax = maxbin.Fit['MPV']/minbin.Fit['MPV']-1.
                 else:
                     rec_sa_minmax = -99
-                q = array('d', [1.*min_percent/100., 1.*max_percent/100.])
+                q = array('d', [1.*self.min_percent/100., 1.*self.max_percent/100.])
                 y = array('d', [0,0])
                 newAnalysis.ExtremeAnalysis.CreateMeanSignalHistogram()
                 newAnalysis.ExtremeAnalysis.MeanSignalHisto.GetQuantiles(2, y, q)
@@ -129,7 +131,7 @@ class MCPerformance(Elementary):
                 remaining_time = estimated_time-elapsed_time
                 print "\n\nAPPROXIMATED TIME LEFT: "+str(remaining_time)+"\n"
             success = 1.*(peaks_generated-fails)/peaks_generated
-            ghost = 4.*ghosts/tries
+            ghost = 4.*ghosts/self.tries
             success_prob.append(success)
             ghost_prob.append(ghost)
 
@@ -140,20 +142,23 @@ class MCPerformance(Elementary):
         infofile.close()
         print "infofile written"
 
-        canvas = ROOT.TCanvas('canvas', 'canvas') # HERE IT CRASHES DUE TO MEMORY PROBLEMS
-        canvas.cd()
-        graph1 = ROOT.TGraph()
-        graph1.SetNameTitle('graph1', 'success')
-        graph1.SaveAs(path+"/SuccessGraph.root")
-        graph2 = ROOT.TGraph()
-        graph2.SetNameTitle('graph2', 'ghosts')
-        graph2.SaveAs(path+"/GhostsGraph.root")
-        for i in xrange(len(heights)):
-            graph1.SetPoint(i, heights[i], success_prob[i])
-            graph2.SetPoint(i, heights[i], ghost_prob[i])
-        graph1.Draw('ALP*')
-        graph2.Draw('SAME LP*')
-        self.SavePlots("PerformanceResult", "png", path+"/")
+        # canvas = ROOT.TCanvas('canvas', 'canvas') # HERE IT CRASHES DUE TO MEMORY PROBLEMS
+        # canvas.cd()
+        # graph1 = ROOT.TGraph()
+        # graph1.SetNameTitle('graph1', 'success')
+        # graph1.SaveAs(path+"/SuccessGraph.root")
+        # graph2 = ROOT.TGraph()
+        # graph2.SetNameTitle('graph2', 'ghosts')
+        # graph2.SaveAs(path+"/GhostsGraph.root")
+        # for i in xrange(len(heights)):
+        #     graph1.SetPoint(i, heights[i], success_prob[i])
+        #     graph2.SetPoint(i, heights[i], ghost_prob[i])
+        # graph1.Draw('ALP*')
+        # graph2.Draw('SAME LP*')
+        # self.SavePlots("PerformanceResult", "png", path+"/")
         answer = raw_input('Wanna crash?')
         if answer == 'yes':
             gc.collect()
+
+    def DoGhostScan(self):
+        pass
