0a2387ad41d47f7504b2cac5e57a3e81d0b154d0
Mario <seelim@student.ethz.ch>
2015-09-09 00:28:38 +0200

    dummyinfo

--- Analyze_August_Testbeam.py
+++ Analyze_August_Testbeam.py
@@ -12,14 +12,13 @@ rateScanCanvas = TCanvas("rateScanCanvas", "rateScanCanvas")
 
 
 sel = RunSelection()
-runplans =sel.runplan[runplanType].keys()
-runplans = ["1"]
+#runplans =sel.runplan[runplanType].keys()
+runplans = map(str, [1,2,11, 12, 13])
 
 for runplanNr in runplans:
     sel.UnselectAll()
-    #sel.SelectRunsFromRunPlan(number=runplanNr, type_=runplanType)      # select all runs from a certain run plan
+    sel.SelectRunsFromRunPlan(number=runplanNr, type_=runplanType)      # select all runs from a certain run plan
     #sel.UnSelectUnlessDiamond(diamondname=diamond)                      # unselect runs, which dont have the diamond in it's channels
-    sel.SelectRunsInRange(75, 77)
     sel.SelectDiamondRuns(diamondname=diamond1, only_selected_runs=True) # activate only a certain diamond for analysis
     sel.SelectDiamondRuns(diamondname=diamond2, only_selected_runs=True)
 
@@ -50,40 +49,3 @@ for runplanNr in runplans:
     rate_analyses[runplanNr].ShowSignalVSRate(canvas=rateScanCanvas)
     sel.SavePlots(savename="SignalVSRate"+str(runplanNr)+".root", canvas=rateScanCanvas)
     sel.SavePlots(savename="SignalVSRate"+str(runplanNr)+".png", canvas=rateScanCanvas)
-
-
-#
-#
-# sel.UnselectAll()
-# sel.SelectRunsInRange(439,459)      # select all runs from a certain run plan
-# sel.SetChannels()
-#
-# print "Selected runs to analyze:"
-# sel.ShowSelectedRuns(commentlength=0)
-#
-# savePath = "Results/new1/"
-# sel.SetSaveDirectory(savePath)
-#
-# rate_analyses[10] = AnalysisCollection(sel)
-#
-# rate_analyses[10].MakePreAnalysis(savePlot=True)
-# rate_analyses[10].ShowPulserRates()
-# rate_analyses[10].ShowSignalVSRate(canvas=rateScanCanvas)
-# sel.SavePlots(savename="SignalVSRate"+str(10)+".root", canvas=rateScanCanvas)
-#
-# sel.UnselectAll()
-# sel.SelectRunsInRange(463,478)      # select all runs from a certain run plan
-# sel.SetChannels()
-#
-# print "Selected runs to analyze:"
-# sel.ShowSelectedRuns(commentlength=0)
-#
-# savePath = "Results/new2/"
-# sel.SetSaveDirectory(savePath)
-#
-# rate_analyses[11] = AnalysisCollection(sel)
-#
-# rate_analyses[11].MakePreAnalysis(savePlot=True)
-# rate_analyses[11].ShowPulserRates()
-# rate_analyses[11].ShowSignalVSRate(canvas=rateScanCanvas)
-# sel.SavePlots(savename="SignalVSRate"+str(11)+".root", canvas=rateScanCanvas)
