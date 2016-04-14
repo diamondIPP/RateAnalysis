31791742c5ca4ecf4ceaab8e44c9bfbd866bcd06
Felix Bachmair <bachmair@phys.ethz.ch>
2016-04-14 09:07:11 +0200

    adding Parser

--- AbstractClasses/show_runs.py
+++ AbstractClasses/show_runs.py
@@ -4,7 +4,7 @@ from sys import argv
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("RunPlan",help = "Displays the deltailed overview about one Runplan")
+parser.add_argument("RunPlan",help = "Displays the deltailed overview about one Runplan", type=int,default = -1)
 
 parser.add_argument("-tc", "--testcampaign", type=int,default=201510,
                     help="TestCampaign: default=201510")
@@ -15,9 +15,7 @@ for arg in args:
 
 raw_input()
 z=RunSelection()
-if len(argv) > 1:
-    z.set_test_campaign(str(args.testcampaign))
-    z.select_runs_from_runplan(argv[1])
-    z.show_selected_runs()
-else:
-    z.show_run_plans()
+z.set_test_campaign(str(args.testcampaign))
+if args.RunPlan != -1:
+    z.select_runs_from_runplan(args.RunPlan)
+z.show_selected_runs()
