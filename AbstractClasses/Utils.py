a66e6f333e0414412c606d49dfe548b5290887d3
michareichmann <micha.reichmann@gmail.com>
2016-05-31 16:35:28 +0200

    adding method to scale the margins of two graphs to the bigger ones

--- AbstractClasses/Utils.py
+++ AbstractClasses/Utils.py
@@ -9,7 +9,15 @@ from termcolor import colored
 # ==============================================
 # UTILITY FUNCTIONS
 # ==============================================
-
 def log_warning(msg):
     t = datetime.now().strftime('%H:%M:%S')
     print '{head} {t} --> {msg}'.format(t=t, msg=msg, head=colored('WARNING:', 'red'))
+
+
+def scale_margins(gr1, gr2):
+    ymin1, ymax1 = gr1.GetYaxis().GetXmin(), gr1.GetYaxis().GetXmax()
+    ymin2, ymax2 = gr2.GetYaxis().GetXmin(), gr2.GetYaxis().GetXmax()
+    ymin = ymin1 if ymin1 < ymin2 else ymin2
+    ymax = ymax1 if ymax1 > ymax2 else ymax2
+    return ymin, ymax
+
