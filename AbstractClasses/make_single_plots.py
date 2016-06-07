cec97bd99ab9164aaf46f11697edff8716eec219
micha <micha.reichmann@gmail.com>
2016-06-07 07:38:08 +0200

    adding script to create a couple of plots for single runs 392 and 398

---
+++ AbstractClasses/make_single_plots.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from PadAnalysis import SignalAnalysis
+from Elementary import Elementary
+
+tc = '201510'
+a = Elementary(tc)
+a.print_banner('STARTING SINGLE PLOT GENERATION')
+a.print_testcampaign()
+
+
+for run in [392, 398]:
+    z = SignalAnalysis(run, 0)
+    z.save_dir = '{tc}_{dia}_{hv}_{rp}'.format(tc=z.TESTCAMPAIGN, dia=z.diamond_name, hv=z.bias, rp=z.run_number)
+    z.draw_trigger_cell_vs_peakpos(show=False)
+    z.draw_bucket_pedestal(show=False)
+    z.draw_peak_timing(show=False)
+    z.show_cut_contributions(flat=True, show=False)
+    z.compare_consecutive_cuts(show=False, save_single=False)
+    z.compare_consecutive_cuts(scale=True, show=False, save_single=False)
+    z.draw_waveforms(20, show=False)
+    z.draw_waveforms(20, cut_string=z.Cut.generate_pulser_cut(), show=False)
+    z.draw_waveforms(5000, show=False)
+    z.draw_intlength_vs_triggercell(show=False)
+    z.draw_peak_integrals(show=False, event=120040)
+    z.draw_cut_means(show=False)
+    z.draw_intlength_vs_triggercell(show=False)
+    z.draw_signal_map(draw_option='colz', show=False)
+
