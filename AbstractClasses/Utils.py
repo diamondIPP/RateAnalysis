692e7608ed8a96da4870790438f2a8ae546646bf
micha <micha.reichmann@gmail.com>
2016-07-28 10:05:25 +0200

    adding method to calculate the flux from a info file

--- AbstractClasses/Utils.py
+++ AbstractClasses/Utils.py
@@ -6,7 +6,7 @@
 from datetime import datetime
 from termcolor import colored
 from ROOT import gStyle
-from math import sqrt
+from numpy import sqrt, mean
 import os
 
 
@@ -214,6 +214,26 @@ def make_latex_table(header, cols):
     return out
 
 
+def calc_flux(info, tc):
+        if 'for1' not in info or info['for1'] == 0:
+            if 'measuredflux' in info:
+                return
+        path = '/data/psi_{0}_{1}/masks/{mask}'.format(tc[:4], tc[-2:], mask=info['maskfile'])
+        if file_exists(path):
+            f = open(path, 'r')
+        else:
+            return
+        data = []
+        for line in f:
+            if len(line) > 3:
+                line = line.split()
+                data.append([int(line[2])] + [int(line[3])])
+        f.close()
+        pixel_size = 0.01 * 0.015
+        area = [(data[1][0] - data[0][0]) * (data[1][1] - data[0][1]) * pixel_size, (data[3][0] - data[2][0]) * (data[3][1] - data[2][1]) * pixel_size]
+        # print area
+        flux = [info['for{0}'.format(i + 1)] / area[i] / 1000. for i in xrange(2)]
+        return mean(flux)
 
 
 def file_exists(path):
