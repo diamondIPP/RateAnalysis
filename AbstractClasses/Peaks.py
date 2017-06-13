20c5c86b0bc66f45a4584f9671aa97af10c6b3df
michareichmann <micha.reichmann@gmail.com>
2017-06-13 09:06:09 +0200

    adding methods to get the total area and spacings of the BCM Lumi Pads

--- AbstractClasses/Peaks.py
+++ AbstractClasses/Peaks.py
@@ -47,3 +47,21 @@ class PeakAnalysis(Elementary):
         print 'Estimated Flux by number of peaks: {f:0.4f} kHz/cm2'.format(f=flux)
         return flux
 
+    def get_area(self):
+        """ return the total area of the BCM' pad sizes """
+        i = int(self.Ana.DiamondName.split('-')[-1]) - 1
+        base_length = 0.0928125  # [cm]
+        spacing = 0.0025
+        radius = 0.0049568
+        rounded_edge = radius ** 2 * (4 - pi)
+        area = base_length ** 2
+        return 2 ** i * area + self.get_spacings(i, spacing, base_length) - rounded_edge
+
+    def get_spacings(self, i, spacing, length):
+        """ return the additional spacings for the BCM' pad sizes """
+        if i > 0:
+            j = 2 ** (i / 2)
+            return spacing * (j * length + (j - 1) * spacing) + 2 * self.get_spacings(i - 1, spacing, length)
+        else:
+            return 0
+
