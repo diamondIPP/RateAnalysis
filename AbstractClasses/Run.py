ec562af378981cfacbe0abf0e8f57d3a1987a53d
michareichmann <micha.reichmann@gmail.com>
2017-06-14 14:58:06 +0200

    removing redundand method calc_flux

--- AbstractClasses/Run.py
+++ AbstractClasses/Run.py
@@ -218,7 +218,7 @@ class Run(Elementary):
 
         self.RunNumber = run_number
         self.load_run_info()
-        self.Flux = self.calc_flux()
+        self.Flux = self.calculate_flux()
 
         # check for conversion
         if load_root_file:
@@ -601,33 +601,6 @@ class Run(Elementary):
         if hasattr(self, 'collection'):
             runs = [self.collection.keys()[0], self.collection.keys()[-1], self.collection.values()[0].run.get_rate_string(), self.collection.values()[-1].run.get_rate_string()]
         return self.draw_run_info(show=False, runs=runs, channel=ch, canvas=pad)
-
-    def calc_flux(self):
-        info = self.RunInfo
-        if 'for1' not in info or info['for1'] == 0:
-            if 'measuredflux' in info:
-                return float(info['measuredflux'])
-        mask = info['maskfile']
-        pixel_size = 0.01 * 0.015
-        if not mask or 'no mask' in mask.lower():
-            area = [52 * 80 * pixel_size] * 2
-        else:
-            path = join(self.maskfilepath, mask)
-            if file_exists(path):
-                f = open(path, 'r')
-            else:
-                log_warning('Could not read maskfile!')
-                return
-            data = []
-            for line in f:
-                if len(line) > 3:
-                    line = line.split()
-                    data.append([int(line[2])] + [int(line[3])])
-            f.close()
-            area = [(data[1][0] - data[0][0]) * (data[1][1] - data[0][1]) * pixel_size, (data[3][0] - data[2][0]) * (data[3][1] - data[2][1]) * pixel_size]
-        # print area
-        flux = [info['for{0}'.format(i + 1)] / area[i] / 1000. for i in xrange(2)]
-        return mean(flux)
 
     def has_branch(self, name):
         return bool(self.tree.GetBranch(name))
