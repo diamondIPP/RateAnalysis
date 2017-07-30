181ee48231ca82bd159a84572150567fdf6605c1
Christian Dorfer <dorfer@phys.ethz.ch>
2017-07-30 03:52:49 +0200

    fixing data dir

--- autoConvert.py
+++ autoConvert.py
@@ -15,7 +15,7 @@ from os.path import realpath, dirname
 
 tc = '201707-2'
 prog_dir = dirname(realpath(__file__))
-data_dir = '/data/psi_{0}_{1}'.format(tc[:4], tc[-2:])
+data_dir = '/data/psi_{0}_{1}'.format(tc[:4], tc[4:])
 raw_dir = '{0}/raw'.format(data_dir)
 final_dir = '{0}/root/pads'.format(data_dir)
 lc = '{dir}/last_converted.txt'.format(dir=prog_dir)
