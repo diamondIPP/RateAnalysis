26cfa88f28d2a7702a9ce93a8e454beff609b79d
michareichmann <micha.reichmann@gmail.com>
2017-09-13 17:30:32 +0200

    adding method to draw cluster size and residuals based on methods in telescopeAnalysis class

--- AbstractClasses/PixAnalysis.py
+++ AbstractClasses/PixAnalysis.py
@@ -729,12 +729,11 @@ class PixAnalysis(Analysis):
         self.save_histo(mg, 'EventOffsets', show, draw_opt='ap', l=l, lm=.13)
         self.reset_colors()
 
-    def draw_cluster_size(self, show=True, cut=''):
-        h = TH1I('h_cs', 'Cluster Size {d}'.format(d=self.DiamondName), 10, 0, 10)
-        self.tree.Draw('clusters_per_plane[{d}]>>h_cs'.format(d=self.Dut), TCut(cut), 'goff')
-        set_statbox(only_entries=True)
-        self.format_histo(h, x_tit='Cluster Size', y_tit='Number of Entries', y_off=1.3, fill_color=self.FillColor)
-        self.save_histo(h, 'ClusterSize', show, logy=True)
+    def draw_cluster_size(self, cut='', show=True):
+        return self._draw_cluster_size(self.Dut, self.DiamondName, cut, show)
+
+    def draw_residuals(self, mode=None, cut=None, show=True, x_range=None):
+        return self._draw_residuals(self.Dut, mode=mode, cut=cut, show=show, x_range=x_range)
 
     # ==========================================================================
 
