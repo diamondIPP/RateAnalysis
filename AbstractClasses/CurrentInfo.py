f288d7093c7182e806e782aaff657c8232592b4c
michareichmann <micha.reichmann@gmail.com>
2018-02-12 14:55:02 +0100

    revising whole graph voltage plot

--- AbstractClasses/CurrentInfo.py
+++ AbstractClasses/CurrentInfo.py
@@ -10,6 +10,7 @@ from ROOT import TCanvas, TText, TGraph
 from ConfigParser import ConfigParser
 from numpy import mean
 from argparse import ArgumentParser
+from time import mktime
 
 from Utils import *
 
@@ -31,7 +32,7 @@ class Currents(Elementary):
 
     def __init__(self, analysis, averaging=False, points=10, dia=None, start_run=None, verbose=False):
         self.Analysis = analysis
-        self.IsCollection = hasattr(analysis, 'runs')
+        self.IsCollection = hasattr(analysis, 'Runs')
         Elementary.__init__(self, verbose=verbose)
 
         self.DoAveraging = averaging
@@ -43,7 +44,6 @@ class Currents(Elementary):
             return
 
         # analysis/run info
-        self.TimeOffset = analysis.run_config_parser.getint('BASIC', 'hvtimeoffset')
         if self.IsCollection:
             self.RunPlan = analysis.RunPlan
         self.RunNumber = self.load_run_number()
@@ -171,16 +171,12 @@ class Currents(Elementary):
         return hv_datapath.format(dev=self.ConfigParser.get('HV' + self.Number, 'name'), ch=self.Channel)
 
     def load_start_time(self):
-        if self.Analysis is not None:
-            return self.Analysis.get_first_analysis().run.log_start + timedelta(hours=self.TimeOffset) if self.IsCollection else self.Analysis.run.log_start + timedelta(hours=self.TimeOffset)
-        else:
-            return None
+        ana = self.Analysis.FirstAnalysis if self.IsCollection else self.Analysis
+        return datetime.fromtimestamp(ana.run.startTime / 1000) if ana is not None else None
 
     def load_stop_time(self):
-        if self.Analysis is not None:
-            return self.Analysis.get_last_analysis().run.log_stop + timedelta(hours=self.TimeOffset) if self.IsCollection else self.Analysis.run.log_stop + timedelta(hours=self.TimeOffset)
-        else:
-            return None
+        ana = self.Analysis.get_last_analysis() if self.IsCollection else self.Analysis
+        return datetime.fromtimestamp(ana.run.endTime / 1000) if ana is not None else None
 
     def set_start_stop(self, sta, sto=None):
         if not sta.isdigit():
@@ -246,12 +242,12 @@ class Currents(Elementary):
     def set_start(self, zero=False):
         self.Currents.append(self.Currents[-1] if not zero else 0)
         self.Voltages.append(self.Voltages[-1] if not zero else 0)
-        self.Time.append((self.StartTime - datetime(self.StartTime.year, 1, 1)).total_seconds())
+        self.Time.append(mktime(self.StartTime.timetuple()))
 
     def set_stop(self, zero=False):
         self.Currents.append(self.Currents[-1] if not zero else 0)
         self.Voltages.append(self.Voltages[-1] if not zero else 0)
-        self.Time.append((self.StopTime - datetime(self.StopTime.year, 1, 1)).total_seconds())
+        self.Time.append(mktime(self.StopTime.timetuple()))
 
     def find_data(self):
         if self.Currents:
@@ -291,7 +287,7 @@ class Currents(Elementary):
             self.set_stop(zero=True)
 
     def save_data(self, now, info, index, shifting=False):
-        total_seconds = (now - datetime(now.year, 1, 1)).total_seconds()
+        # total_seconds = (now - datetime(now.year, 1, 1)).total_seconds()
         if self.StartTime < now < self.StopTime and float(info[2]) < 1e30:
             index += 1
             if self.DoAveraging:
@@ -301,7 +297,7 @@ class Currents(Elementary):
                     if index % self.Points == 0:
                         if mean(self.Currents) < 5 * self.MeanCurrent / self.Points:
                             self.Currents.append(self.MeanCurrent / self.Points)
-                            self.Time.append(total_seconds)
+                            self.Time.append(mktime(now.timetuple()))
                             self.Voltages.append(self.MeanVoltage / self.Points)
                             self.MeanCurrent = 0
                             self.MeanVoltage = 0
@@ -322,7 +318,7 @@ class Currents(Elementary):
                         if abs(self.Currents[-1]) > 0.01:
                             return
                 self.Currents.append(float(info[2]) * 1e9)
-                self.Time.append(total_seconds)
+                self.Time.append(mktime(now.timetuple()))
                 self.Voltages.append(float(info[1]))
 
     def find_start(self, data, log_date):
@@ -359,35 +355,39 @@ class Currents(Elementary):
 
     # ==========================================================================
     # region PLOTTING
-    def set_graphs(self, rel_time=True):
+    def set_graphs(self):
+        if self.Currents:
+            return
         self.find_data()
-        self.convert_to_relative_time() if rel_time else self.do_nothing()
         sleep(.1)
         self.make_graphs()
         self.set_margins()
 
+    def draw_distribution(self, show=True):
+        pass
+
     def draw_indep_graphs(self, rel_time=False, ignore_jumps=True, v_range=None, show=True):
         self.IgnoreJumps = ignore_jumps
-        if not self.Currents:
-            self.set_graphs(rel_time)
+        self.set_graphs()
         self.set_root_output(show)
         c = TCanvas('c', 'Keithley Currents for Run {0}'.format(self.RunNumber), int(self.Res * 1.5), int(self.Res * .75))
         self.draw_voltage_pad(v_range)
         self.draw_title_pad()
-        self.draw_current_pad()
+        self.draw_current_pad(rel_t=rel_time)
 
         self.Stuff.append(c)
         self.save_plots('{dia}_{bias}'.format(dia=self.DiamondName, bias=self.Bias), sub_dir='Currents', show=show)
 
-    def draw_current_pad(self):
-        pad = self.draw_tpad('p3', gridx=True, margins=[.1, .09, .15, .15], transparent=True)
-        self.draw_current_frame(pad)
+    def draw_current_pad(self, rel_t):
+        pad = self.draw_tpad('p3', gridx=True, margins=[.065, .09, .15, .15], transparent=True)
+        self.draw_current_frame(pad, rel_t)
         self.CurrentGraph.Draw('pl')
 
     def draw_voltage_pad(self, vrange):
-        pad = self.draw_tpad('p1', gridy=True, margins=[.1, .09, .15, .15])
+        pad = self.draw_tpad('p1', gridy=True, margins=[.065, .09, .15, .15])
         self.draw_voltage_frame(pad, vrange)
         self.VoltageGraph.Draw('p')
+        self.VoltageGraph.GetXaxis().SetRangeUser(self.Time[0], self.Time[-1])
         self.draw_voltage_axis(vrange)
 
     def draw_title_pad(self):
@@ -431,7 +431,7 @@ class Currents(Elementary):
 
     def draw_voltage_axis(self, vrange):
         vrange = [-1100, 1100] if vrange is None else vrange
-        a1 = self.draw_y_axis(self.Margins['x'][1], vrange[0], vrange[1], '#font[22]{Voltage [V]}', col=col_vol, off=title_offset,
+        a1 = self.draw_y_axis(self.Time[-1], vrange[0], vrange[1], '#font[22]{Voltage [V]}', col=col_vol, off=title_offset,
                               tit_size=axis_title_size, opt='+L', w=2, lab_size=label_size, l_off=.01)
         a1.CenterTitle()
 
@@ -445,20 +445,18 @@ class Currents(Elementary):
         x = self.Margins['x'][0] - (self.Margins['x'][1] - self.Margins['x'][0]) * .07
         self.draw_y_axis(x, ymin, ymax, 'pulse height [au]', off=.8, tit_size=.04, opt='-R', w=2, col=859, lab_size=label_size, l_off=.01)
 
-    def draw_current_frame(self, pad):
+    def draw_current_frame(self, pad, rel_t):
         m = self.Margins
         h2 = pad.DrawFrame(m['x'][0], m['y'][0], m['x'][1], m['y'][1])
         # X-axis
         h2.GetXaxis().SetTitle("#font[22]{Time [hh:mm]}")
-        h2.GetXaxis().SetTimeFormat("%H:%M")
-        h2.GetXaxis().SetTimeOffset(-3600)
-        h2.GetXaxis().SetTimeDisplay(1)
         h2.GetXaxis().SetLabelSize(label_size)
         h2.GetXaxis().SetTitleSize(axis_title_size)
         h2.GetXaxis().SetTitleOffset(1.05)
         h2.GetXaxis().SetTitleSize(0.05)
+        set_time_axis(h2, off=self.Time[0] if rel_t else 0)
         # Y-axis
-        h2.GetYaxis().SetTitleOffset(title_offset)
+        h2.GetYaxis().SetTitleOffset(.55)
         h2.GetYaxis().SetTitleSize(0.05)
         h2.GetYaxis().SetTitle("#font[22]{Current [nA]}")
         h2.GetYaxis().SetTitleColor(col_cur)
@@ -467,6 +465,7 @@ class Currents(Elementary):
         h2.GetYaxis().CenterTitle()
         h2.GetYaxis().SetLabelSize(label_size)
         h2.GetYaxis().SetTitleSize(axis_title_size)
+        h2.GetXaxis().SetRangeUser(self.Time[0], self.Time[-1])
         # self.Stuff.append(h2)
 
     def draw_ph_frame(self, pad, margins):
@@ -499,21 +498,8 @@ class Currents(Elementary):
         h1.GetXaxis().SetLabelOffset(99)
         h1.GetYaxis().SetLabelOffset(99)
         h1.SetLineColor(0)
+        h1.GetXaxis().SetRangeUser(self.Time[0], self.Time[-1])
 
-    def draw_curr_frame(self, pad):
-        pad.cd()
-        m = self.Margins
-        y = m['y']
-        yadd = (y[1] - y[0]) * .1
-        diff = (m['y'][1] - m['y'][0])
-        h1 = pad.DrawFrame(m['x'][0], m['y'][0] - yadd, m['x'][1], m['y'][1] + diff + yadd)
-        h1.SetTitleSize(axis_title_size)
-        h1.GetXaxis().SetTickLength(0)
-        h1.GetYaxis().SetTickLength(0)
-        h1.GetXaxis().SetLabelOffset(99)
-        h1.GetYaxis().SetLabelOffset(99)
-        h1.SetLineColor(0)
-        
     def draw_pulser_frame(self, pad, ymin, ymax):
         pad.cd()
         m = self.Margins
