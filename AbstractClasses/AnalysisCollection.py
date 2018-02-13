cf693266afbc10ca3c157449d62d7bed7cfa5b29
michareichmann <micha.reichmann@gmail.com>
2018-02-13 11:08:28 +0100

    fix time binning if timestamp resets after one day

--- AbstractClasses/AnalysisCollection.py
+++ AbstractClasses/AnalysisCollection.py
@@ -1377,6 +1377,9 @@ class AnalysisCollection(Elementary):
 
     def get_t_binning(self, evts_per_bin):
         binnings = [ana.get_time_bins(evts_per_bin) for ana in self.collection.itervalues()]
+        for i in xrange(1, len(binnings[1])):
+            if binnings[1][i] < binnings[1][i - 1]:
+                binnings[1][i] += timedelta(days=1).total_seconds()
         return [sum(ibin[0] for ibin in binnings), concatenate([ibin[1] for ibin in binnings])]
 
     def set_channel(self, ch):
