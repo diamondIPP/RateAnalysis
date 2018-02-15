b3152d44eef18f2dd6bbc0f11e24ea8d820dde26
michareichmann <micha.reichmann@gmail.com>
2018-02-15 13:36:56 +0100

    setting start and end time from timestamps to seconds

--- AbstractClasses/Run.py
+++ AbstractClasses/Run.py
@@ -205,10 +205,10 @@ class Run(Elementary):
         return datetime.strptime(t, '%Y-%m-%dT%H:%M:%SZ' if 'Z' in t else '%H:%M:%S') + timedelta(hours=self.run_config_parser.getint('BASIC', 'hvtimeoffset'))
 
     def load_start_time(self):
-        return self.get_time_at_event(self.startEvent) / 1000
+        return int(round(self.get_time_at_event(self.startEvent) / 1000))
 
     def load_end_time(self):
-        return self.get_time_at_event(self.endEvent) / 1000
+        return int(round(self.get_time_at_event(self.endEvent) / 1000))
 
     def load_n_planes(self):
         if self.has_branch('cluster_col'):
