a4acf72b03047700d263f98548deff4a1fb209f7
michareichmann <micha.reichmann@gmail.com>
2018-02-21 14:27:13 +0100

    small fix

--- AbstractClasses/AnalysisCollection.py
+++ AbstractClasses/AnalysisCollection.py
@@ -273,7 +273,7 @@ class AnalysisCollection(Elementary):
     def draw_full_pulse_height(self, evts_per_bin=10000, show=True, rel_t=True, redo=False, with_flux=True):
         histos = [ana.draw_pulse_height(evts_per_bin, corr=True, redo=redo, show=False)[0] for ana in self.collection.itervalues()]
         h1 = TH1F('hfph', 'Pulse Height for Run Plan {n}'.format(n=self.RunPlan), *self.get_t_binning(evts_per_bin))
-        i_bin = 1  # the first bin is empty
+        i_bin = 0  # the first bin is empty
         for h in histos:
             for i in xrange(1, h.GetNbinsX() + 1):
                 h1.SetBinContent(i_bin + 1, h.GetBinContent(i))
