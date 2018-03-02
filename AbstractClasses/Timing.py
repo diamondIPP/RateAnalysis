d7d8bcc0e9c70dfeaafa16a7640a97b309ce38d0
michareichmann <micha.reichmann@gmail.com>
2018-03-02 14:36:33 +0100

    adding methods for scint

--- AbstractClasses/Timing.py
+++ AbstractClasses/Timing.py
@@ -81,14 +81,18 @@ class TimingAnalysis(Elementary):
         self.format_histo(h, x_tit='Signal Peak Timing [ns]', y_tit='RF Phase [ns]', z_tit='Number of Entries', z_off=1.2, y_off=1.2, stats=0, x_range=x_range, y_range=y_range)
         self.save_histo(h, 'RFSignalPeak', rm=.18, draw_opt='colz', show=show)
 
-    def draw_inflexion_time(self, corr=False, show=True):
+    def draw_inflexion_time(self, corr=False, channel=None, show=True):
+        channel = self.Ana.channel if channel is None else channel
         h = TH1F('hrt', 'Inflexion Time', 2000 * (2 if corr else 1), 0, 500)
         corr = '+rf_phase' if corr else ''
-        self.Tree.Draw('rise_time[{ch}]{c}>>hrt'.format(ch=self.Ana.channel, c=corr), self.Cut.all_cut, 'goff')
+        self.Tree.Draw('rise_time[{ch}]{c}>>hrt'.format(ch=channel, c=corr), self.Cut.all_cut + TCut('rise_time[{ch}]'.format(ch=channel)), 'goff')
         set_statbox(w=.3, entries=6)
         x_range = increased_range([h.GetBinCenter(ibin) for ibin in [h.FindFirstBinAbove(5), h.FindLastBinAbove(5)]], .1, .3)
         self.format_histo(h, x_tit='Rise Time [ns]', y_tit='Number of Entries', y_off=1.2, fill_color=self.FillColor, x_range=x_range)
         self.draw_histo(h, show=show, lm=.12)
+
+    def draw_scint_inflexion(self, corr=False, show=True):
+        return self.draw_inflexion_time(corr=corr, channel=self.get_scint_channel(), show=show)
 
     def draw_peak_width(self, show=True):
         h = TH1F('hpw', 'Peak Width', 500, 0, 1)
@@ -98,12 +102,18 @@ class TimingAnalysis(Elementary):
         self.format_histo(h, x_tit='Rise Width [au]', y_tit='Number of Entries', y_off=1.2, fill_color=self.FillColor, x_range=x_range)
         self.draw_histo(h, show=show, lm=.12)
 
+    def draw_threshold(self, corr=False, channel=None, show=True):
+        channel = self.Ana.channel if channel is None else channel
         h = TH1F('hrt', 'Signal over Threshold', 2000 * (2 if corr else 1), 0, 500)
         corr = '+rf_phase' if corr else ''
+        self.Tree.Draw('t_thresh[{ch}]{c}>>hrt'.format(ch=channel, c=corr), self.Cut.all_cut + TCut('t_thresh[{ch}] > 10'.format(ch=channel)), 'goff')
         set_statbox(w=.3, entries=6)
         x_range = increased_range([h.GetBinCenter(ibin) for ibin in [h.FindFirstBinAbove(5), h.FindLastBinAbove(5)]], .1, .3)
         self.format_histo(h, x_tit='Threshold Time [ns]', y_tit='Number of Entries', y_off=1.2, fill_color=self.FillColor, x_range=x_range)
         self.draw_histo(h, show=show, lm=.12)
+
+    def draw_scint_threshold(self, corr=False, show=True):
+        return self.draw_threshold(corr=corr, channel=self.get_scint_channel(), show=show)
 
     def get_channel_nr(self, name):
         try:
