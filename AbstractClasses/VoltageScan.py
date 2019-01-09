08e0f2e3cebc2b5fe7a6f25c5572f2e8fe296f91
michareichmann <micha.reichmann@gmail.com>
2019-01-09 22:01:19 +0100

    revise draw_pulse_height

--- AbstractClasses/VoltageScan.py
+++ AbstractClasses/VoltageScan.py
@@ -56,52 +56,29 @@ class VoltageScan(Elementary):
     def draw_pulser_pulse_height(self, binning=10000, redo=False, show=True):
         return self.draw_pulse_height(binning, pulser=True, redo=redo, show=show)
 
-    def draw_pulse_height(self, binning=30000, pulser=False, redo=False, show=True, cut=None):
-        gr1 = self.make_tgrapherrors('gStatError', 'stat. error', self.get_color())
+    def draw_pulse_height(self, binning=None, pulser=False, redo=False, first_last=True, show=True):
+
+        marker_size = 1
         gStyle.SetEndErrorSize(4)
-        gr_first = self.make_tgrapherrors('gFirst', 'first run', marker=22, color=2, marker_size=2)
-        gr_last = self.make_tgrapherrors('gLast', 'last run', marker=23, color=2, marker_size=2)
-        gr_errors = self.make_tgrapherrors('gFullError', 'stat. + repr. error', marker=0, color=602, marker_size=0)
-
-        # flux_errors = self.draw_ph_distributions_below_flux(flux=80, show=False, save_plot=False)
-        # rel_sys_error = flux_errors[1] / flux_errors[0]
-        rel_sys_error = 0
-        i, j = 0, 0
-        for key, ana in self.collection.iteritems():
-            try:
-                fit1 = ana.draw_pulse_height(bin_size=binning, corr=True, save=redo, show=False, redo=redo)[1] if not pulser else ana.Pulser.draw_distribution_fit(show=False, prnt=False)
-            except TypeError:
-                fit1 = ana.draw_pulse_height(bin_size=binning, cut=cut, show=False)
-            x = ana.Run.RunInfo['dia{nr}hv'.format(nr=self.DiamondNumber)]
-            s, e = (fit1.Parameter(0), fit1.ParError(0)) if not pulser else (fit1.Parameter(1), fit1.ParError(1))
-            gr1.SetPoint(i, x, s)
-            self.log_info('{x}\t{s:5.2f} {e:3.2f}'.format(x=x, s=s, e=e))
-            gr1.SetPointError(i, 0, e)
-            gr_errors.SetPoint(i, x, s)
-            gr_errors.SetPointError(i, 0, e + rel_sys_error * s)
-            # set special markers for the first and last run
-            if i == 0:
-                gr_first.SetPoint(0, x, s)
-            if j == len(self.collection) - 1:
-                gr_last.SetPoint(0, x, s)
-            i += 1
-            j += 1
-        graphs = [gr_errors, gr1]
-        gr_line = gr1.Clone()
-        self.format_histo(gr_line, name='gLine', color=920)
-        graphs += [gr_first, gr_last]
-        legend = self.make_legend(.65, .35, nentries=len(graphs))
-        # gr1.SetName('data') if len(graphs) < 5 else self.do_nothing()
-
-        mg = TMultiGraph('mg_ph', '' + self.DiamondName)
-        # mg.Add(gr_line, 'l')
+        ph = self.Ana.Pulser.get_pulse_heights(redo=redo) if pulser else self.Ana.get_pulse_heights(binning, redo)
+        y_values = [dic['ph'] for dic in ph.itervalues()]
+        x_values = self.get_voltages()
+        g = self.make_tgrapherrors('gStatError', 'stat. error', self.colors[2], x=self.get_voltages(), y=y_values, marker_size=marker_size)
+        rel_sys_error = 0  # TODO think of something else here...
+        y_values = [make_ufloat((v.n, v.s + rel_sys_error * v.n)) for v in y_values]
+        g_errors = self.make_tgrapherrors('gerr', 'full error', marker=0, color=602, marker_size=0, x=x_values, y=y_values)
+        g_first = self.make_tgrapherrors('g1', 'first run', marker=22, color=2, marker_size=marker_size, x=[x_values[0].n], y=[y_values[0].n])
+        g_last = self.make_tgrapherrors('g2', 'last run', marker=23, color=2, marker_size=marker_size, x=[x_values[-1].n], y=[y_values[-1].n])
+        graphs = [g, g_errors]
+        graphs += [g_first, g_last] if first_last else []
+        l = self.make_legend(.75, .37, nentries=len(graphs))
+        mg = TMultiGraph('mg_ph', 'Pulse Height - {dia}'.format(dia=self.DiamondName))
         for gr in graphs:
-            if gr.GetName().startswith('gFull'):
-                legend.AddEntry(gr, gr.GetTitle(), 'l')
-            else:
-                legend.AddEntry(gr, gr.GetTitle(), 'p')
-            mg.Add(gr, 'p')
-        max_y = max(graphs[0].GetY()[i] for i in xrange(graphs[0].GetN()))
-        self.format_histo(mg, x_tit='Voltage [V]', y_tit='Pulse Height [au]', y_off=2, draw_first=True, y_range=[0, max_y * 1.2])
-        self.save_histo(mg, '{s}VoltageScan'.format(s='Signal' if not pulser else 'Pulser'), draw_opt='a', lm=.14, show=show)
+            l.AddEntry(gr, gr.GetTitle(), 'l' if gr.GetName() == 'gerr' else 'p')
+            mg.Add(gr, 'pl')
+        mg.GetListOfFunctions().Add(l)
         self.reset_colors()
+        max_y = max(graphs[0].GetY()[i] for i in xrange(graphs[0].GetN()))
+        self.format_histo(mg, x_tit='Voltage [V]', y_tit='Pulse Height [au]', y_off=1.4, draw_first=True, y_range=[0, max_y * 1.2])
+        self.save_histo(mg, '{s}VoltageScan'.format(s='Signal' if not pulser else 'Pulser'), draw_opt='a', lm=.14, show=show)
+        return mg
