e7a1451428f0e9ec677fd1290b86a67ad390a9a9
michareichmann <micha.reichmann@gmail.com>
2019-02-06 11:41:04 +0100

    add option redo to copy_raw_file()

--- AbstractClasses/Converter.py
+++ AbstractClasses/Converter.py
@@ -236,8 +236,8 @@ class Converter:
         polarities = [sign(biases.pop(0)) * fac if has_bit(active_regions, i) else 0 for i in xrange(self.NChannels)]
         return str([(1 if not pol and has_bit(active_regions, i) else pol) for i, pol in enumerate(polarities)])  # pol cannot be 0, just take 1 for 0V
 
-    def copy_raw_file(self):
-        if not file_exists(self.make_raw_file_path()):
+    def copy_raw_file(self, redo=False):
+        if not file_exists(self.make_raw_file_path()) or redo:
             main_data_path = join('isg:', 'home', 'ipp', self.TcDir, 'raw', basename(self.make_raw_file_path()))
             log_message('Trying to copy {}'.format(basename(self.make_raw_file_path())))
             system('rsync -aPv {} {}'.format(main_data_path, self.RawFileDir))
