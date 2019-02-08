97e21706ab47bdd3159a4faee1c40bbff073c0b8
michareichmann <micha.reichmann@gmail.com>
2019-02-08 07:00:01 +0100

    small fix

--- AbstractClasses/PadAlignment.py
+++ AbstractClasses/PadAlignment.py
@@ -97,7 +97,7 @@ class PadAlignment:
     def get_pulser_rate(self):
         h = TProfile('hprc', 'Pulser Rate', self.NEntries / 500, 0, self.NEntries)
         self.InTree.Draw('pulser:Entry$>>hprc', '', 'goff')
-        values = [make_ufloat((h.GetBinContent(i), 1 / h.GetBinError(i))) for i in xrange(1, h.GetNbinsX() + 1) if h.GetBinContent(i)]
+        values = [make_ufloat((h.GetBinContent(i), 1 / h.GetBinError(i))) for i in xrange(1, h.GetNbinsX() + 1) if h.GetBinContent(i) and h.GetBinError(i)]
         raw_mean = mean(values)
         values = [v for v in values if v < raw_mean + .1]
         m, s = mean_sigma(values)
