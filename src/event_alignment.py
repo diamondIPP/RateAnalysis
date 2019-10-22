0f218520a5a309422c1f51f3cb108501f9e856c9
michareichmann <micha.reichmann@gmail.com>
2019-10-22 16:40:07 +0200

    adding event_alignment base class, complete revision of padaligment and small revision of pixalignment

---
+++ src/event_alignment.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# --------------------------------------------------------
+#       general class for event alignment
+# created on October 18th 2019 by M. Reichmann (remichae@phys.ethz.ch)
+# --------------------------------------------------------
+
+from utils import *
+from ROOT import TFile, vector
+from progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar
+from numpy import split, cumsum
+
+
+class EventAligment:
+    def __init__(self, converter):
+
+        # Time
+        self.StartTime = time()
+
+        # Converter
+        self.Converter = converter
+        self.Run = converter.Run
+
+        # ROOT Files and Trees
+        self.InFile = self.load_file()
+        self.InTree = self.InFile.Get(self.Run.TreeName)
+        self.NewFile = None
+        self.NewTree = None
+
+        # Info
+        self.NEntries = int(self.InTree.GetEntries())
+        self.MaxOffset = 5
+        self.AtEntry = -1
+        self.IsAligned = self.check_alignment_fast()
+
+        # Branches
+        self.Branches = self.init_branches()
+        self.Variables = None
+        self.NHits = None
+
+        # Progress Bar
+        self.Widgets = ['Progress: ', Percentage(), ' ', Bar(marker='>'), ' ', ETA(), ' ', FileTransferSpeed()]
+        self.ProgressBar = None
+
+    def __del__(self):
+        self.InFile.Close()
+        print_elapsed_time(self.StartTime, 'Pad Alignment')
+
+    def __call__(self):
+        self.run()
+
+    def run(self):
+        if not self.IsAligned:
+            self.print_start()
+            self.NHits = self.load_n_hits()
+            self.Variables = self.load_variables()
+            self.update_variables()
+            if not self.check_alignment():  # make detailed check
+                self.write_aligned_tree()
+                if file_exists(self.Converter.ErrorFile):
+                    remove(self.Converter.ErrorFile)  # file is not needed anymore and just gets appended otherwise
+
+    # ----------------------------------------
+    # region INIT
+    def print_start(self):
+        pass
+
+    def load_file(self):
+        f_name = self.Converter.get_eudaqfile_path()
+        if f_name is None:
+            self.Converter.convert_raw_to_root()
+        return TFile(f_name)
+
+    def check_alignment(self):
+        return self.IsAligned
+
+    def check_alignment_fast(self):
+        pass
+
+    @staticmethod
+    def init_branches():
+        dic = OrderedDict()
+        dic['plane'] = vector('unsigned short')()
+        dic['col'] = vector('unsigned short')()
+        dic['row'] = vector('unsigned short')()
+        dic['adc'] = vector('short')()
+        dic['charge'] = vector('unsigned int')()
+        return dic
+
+    def load_n_hits(self, n_entries=None, first_entry=0):
+        n = self.InTree.Draw('@plane.size()', '', 'goff', self.NEntries if n_entries is None else n_entries, first_entry)
+        return get_root_vec(self.InTree, n, dtype='u1')
+
+    def load_variables(self):
+        """ get all the telescope branches in vectors"""
+        t = self.Run.info('Loading information from tree ... ', next_line=False)
+        n_hits = self.InTree.Draw('plane', '', 'goff')
+        self.InTree.SetEstimate(n_hits)
+        self.InTree.Draw('plane:col:row:adc:charge', '', 'paragoff')
+        plane, column, row, adc, charge = get_root_vecs(self.InTree, n_hits, 5, dtype=int)
+        self.Run.add_to_info(t)
+        return split(array([plane, column, row, adc, charge]), cumsum(self.NHits), axis=1)
+
+    def reload(self):
+        self.NHits = self.load_n_hits()
+        self.Variables = self.load_variables()
+        self.update_variables()
+
+    def update_variables(self):
+        """ get additional vectors"""
+        pass
+    # endregion INIT
+    # ----------------------------------------
+
+    # ----------------------------------------
+    # region OFFSETS
+
+    def find_start_offset(self):
+        return 0
+
+    def find_final_offset(self):
+        return 0
+
+    def load_error_offsets(self):
+        self.Run.info('Using decoding errors to get the event alignment offsets')
+        zero_offset = [(0, self.find_start_offset())] if self.find_start_offset() else []
+        return OrderedDict(zero_offset + [(event_number, 1) for event_number in self.Converter.DecodingErrors])
+
+    def find_manual_offsets(self):
+        return {}
+
+    def find_offsets(self):
+        use_decoding_errors = (self.find_final_offset() == len(self.Converter.DecodingErrors) + self.find_start_offset()) and self.Converter.DecodingErrors.size
+        errors = self.load_error_offsets() if use_decoding_errors else self.find_manual_offsets()
+        info('Found {n} offsets'.format(n=len(errors)))
+        return errors
+
+    # endregion OFFSETS
+    # ----------------------------------------
+
+    # ----------------------------------------
+    # region WRITE TREE
+    def get_next_event(self):
+        if self.AtEntry == self.NEntries:
+            return False
+        self.AtEntry += 1
+        self.InTree.GetEntry(self.AtEntry)
+        return True
+
+    def set_branch_addresses(self):
+        for name, branch in self.Branches.iteritems():
+            self.NewTree.SetBranchAddress(name, branch)
+
+    def save_tree(self):
+        self.NewFile.cd()
+        self.NewTree.Write()
+        macro = self.InFile.Get('region_information')
+        if macro:
+            macro.Write()
+        self.NewFile.Write()
+        self.Run.info('successfully aligned the tree and saved it to {}'.format(self.NewFile.GetName()))
+
+    def clear_vectors(self):
+        for vec in self.Branches.itervalues():
+            vec.clear()
+
+    def show_branches(self):
+        for i,j in self.Branches.iteritems():
+            print i, list(j)
+
+    def fill_branches(self, offset):
+        pass
+
+    def write_aligned_tree(self):
+        offsets = self.find_offsets()
+        self.NewFile = TFile(self.Converter.get_eudaqfile_path(), 'RECREATE')
+        self.NewTree = self.InTree.CloneTree(0)
+        self.set_branch_addresses()
+        self.start_pbar(self.NEntries)
+        offset = 0
+        while self.get_next_event():
+            self.ProgressBar.update(self.AtEntry + 1)
+            self.clear_vectors()
+            if self.AtEntry in offsets:
+                offset += offsets[self.AtEntry]
+            if self.AtEntry > self.NEntries - abs(offset) - 1:
+                break
+            self.fill_branches(offset)
+            self.NewTree.Fill()
+        self.ProgressBar.finish()
+        self.save_tree()
+    # endregion WRITE TREE
+    # ----------------------------------------
+
+    def start_pbar(self, n):
+        self.ProgressBar = ProgressBar(widgets=self.Widgets, maxval=n)
+        self.ProgressBar.start()
+
+
+if __name__ == '__main__':
+
+    from pad_run import PadRun
+    from converter import Converter
+
+    args = init_argparser(run=23, tc='201908')
+    zrun = PadRun(args.run, test_campaign=args.testcampaign, tree=False, verbose=True)
+    z = EventAligment(Converter(zrun))
--- src/pix_alignment.py
+++ src/pix_alignment.py
@@ -4,124 +4,64 @@
 # created on February 13th 2017 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
 
-from ROOT import TFile, TH1F, vector
-from collections import OrderedDict, Counter
-from numpy import mean, sqrt
-from Utils import set_root_output, log_message, time, print_elapsed_time, calc_mean, round_up_to
-from progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar
+from ROOT import TH1F
+from numpy import linspace, split, count_nonzero, where, cumsum, concatenate
+
 from Correlation import Correlation
-from os import remove
+from event_alignment import EventAligment
+from utils import *
 
 
-class PixAlignment:
+class PixAlignment(EventAligment):
     def __init__(self, converter):
-        # main
-        self.StartTime = time()
-        self.Converter = converter
-        self.Run = converter.Run
-        self.NDutPlanes = 4
+
+        # Info
         self.Threshold = .4
-        # progress bar
-        self.Widgets = ['Progress: ', Percentage(), ' ', Bar(marker='>'), ' ', ETA(), ' ', FileTransferSpeed()]
-        self.ProgressBar = None
-        # files/trees
-        self.InFile = TFile(converter.get_root_file_path())
-        self.InTree = self.InFile.Get(self.Run.treename)
-        self.NewFile = None
-        self.NewTree = None
-        # alignment
-        try:
-            self.NEntries = int(self.InTree.GetEntries())
-        except AttributeError:
-            remove(self.Converter.get_root_file_path())
-        self.AtEntry = 0
-        self.IsAligned = self.check_alignment_fast()
-        if not self.IsAligned:
-            # branches
-            self.Branches = self.init_branches()
-            self.BranchLists = {name: [] for name in self.Branches}
-            # info
-            self.TelRow = {}
-            self.DiaRow = {}
-            self.load_variables()
-            self.BucketSize = self.find_bucket_size(show=False)
+        self.NDutPlanes = 4
+        self.TelPlane = 2
+        self.DUTPlane = 4
 
-    def __del__(self):
-        self.InFile.Close()
-        print_elapsed_time(self.StartTime, 'Pixel Alignment')
+        EventAligment.__init__(self, converter)
 
-    @staticmethod
-    def init_branches():
-        dic = OrderedDict()
-        dic['plane'] = vector('unsigned short')()
-        dic['col'] = vector('unsigned short')()
-        dic['row'] = vector('unsigned short')()
-        dic['adc'] = vector('short')()
-        dic['charge'] = vector('unsigned int')()
-        return dic
+        self.TelRow = {}
+        self.DUTRow = {}
+        self.BinSize = None
 
-    def load_variables(self):
-        t = self.Run.log_info('Loading information from tree ... ', next_line=False)
-        self.InTree.SetEstimate(self.InTree.Draw('plane', '', 'goff'))
-        x, y = OrderedDict(), OrderedDict()
-        p1, p2 = 2, 4
-        dic = {name: None for name in self.BranchLists}
-        n = self.InTree.Draw('plane:row:col:event_number', '', 'goff')
-        dic['plane'] = [int(self.InTree.GetV1()[i]) for i in xrange(n)]
-        dic['row'] = [int(self.InTree.GetV2()[i]) for i in xrange(n)]
-        dic['col'] = [int(self.InTree.GetV3()[i]) for i in xrange(n)]
-        nrs = Counter([int(self.InTree.GetV4()[i]) for i in xrange(n)])
-        n = self.InTree.Draw('adc:charge', '', 'goff')
-        dic['adc'] = [int(self.InTree.GetV1()[i]) for i in xrange(n)]
-        dic['charge'] = [int(self.InTree.GetV2()[i]) for i in xrange(n)]
-        n_ev = 0
-        at_event = 0
-        for ev, size in sorted(nrs.iteritems()):
-            # account for events that have no pixel information
-            while at_event != ev:
-                for name in dic.iterkeys():
-                    self.BranchLists[name].append([])
-                at_event += 1
-            for name, lst in dic.iteritems():
-                self.BranchLists[name].append(lst[n_ev:size + n_ev])
-            plane = dic['plane'][n_ev:size + n_ev]
-            row = dic['row'][n_ev:size + n_ev]
-            if plane.count(p1) == 1:
-                x[ev] = row[plane.index(p1)]
-            if plane.count(p2) == 1:
-                y[ev] = row[plane.index(p2)]
-            n_ev += size
-            at_event += 1
-        self.Run.add_info(t)
-        self.TelRow = x
-        self.DiaRow = y
+    def print_start(self):
+        print_banner('STARTING PIXEL EVENT ALIGNMENT OF RUN {}'.format(self.Converter.RunNumber))
+
+    def update_variables(self):
+        """Find all events with have both hits in the two checked planes"""
+        t = self.Run.info('Loading pixel information from tree ... ', next_line=False)
+        for i, ev in enumerate(self.Variables):
+            plane, row = ev[0], ev[2]
+            if count_nonzero(plane == self.TelPlane) == 1:
+                self.TelRow[i] = row[where(plane == self.TelPlane)][0]
+            if count_nonzero(plane == self.DUTPlane) == 1:
+                self.DUTRow[i] = row[where(plane == self.DUTPlane)][0]
+        self.BinSize = self.find_bucket_size(show=False)
+        self.Run.add_to_info(t)
 
     def check_alignment_fast(self):
-        """ only check alignment of the first, last and middle 10k events """
-        t = self.Run.log_info('Fast check for event alignment ... ', next_line=False)
+        """ only check alignment for subsets of 10k events """
+        t = self.Run.info('Fast check for event alignment ... ', next_line=False)
         corrs = []
-        for start_event in [self.NEntries / 10 * i for i in xrange(9)] + [self.NEntries - 10000]:
+        for start_event in linspace(0, self.NEntries, 10, endpoint=False, dtype='i4'):
             correlation = Correlation(self, bucket_size=10000)
-            n = self.InTree.Draw('plane:row:event_number', '', 'goff', 10000, start_event)
-            planes = [int(self.InTree.GetV1()[i]) for i in xrange(n)]
-            rows = [int(self.InTree.GetV2()[i]) for i in xrange(n)]
-            nrs = Counter([int(self.InTree.GetV3()[i]) for i in xrange(n)])
-            n_ev = 0
-            for ev, size in sorted(nrs.iteritems()):
-                plane = planes[n_ev:size + n_ev]
-                row = rows[n_ev:size + n_ev]
-                if plane.count(2) == 1 and plane.count(4) == 1:
-                    correlation.fill(ev, tel_row=row[plane.index(2)], dia_row=row[plane.index(4)])
-                n_ev += size
-            corrs.append(correlation.get(debug=False))
+            n = self.InTree.Draw('plane:row', '', 'goff', 10000, start_event)
+            planes, rows = get_root_vecs(self.InTree, n, 2, dtype='u1')
+            n_hits = self.load_n_hits(10000, start_event)
+            for i, (plane, row) in enumerate(split(array([planes, rows]), cumsum(n_hits), axis=1)):
+                if count_nonzero(plane == self.TelPlane) == 1 and count_nonzero(plane == self.DUTPlane) == 1:  # if both planes have exactly one hit
+                    correlation.fill(i, tel_row=row[where(plane == self.TelPlane)][0], dia_row=row[where(plane == self.DUTPlane)][0])
+            corrs.append(correlation(debug=False))
         is_aligned = all(corr > self.Threshold for corr in corrs)
-        self.Run.add_info(t)
-        if not is_aligned:
-            self.Run.log_info('Fast check found misalignment :-(')
+        self.Run.add_to_info(t)
+        self.Run.info('Run {} is perfectly aligned :-)'.format(self.Run.RunNumber) if is_aligned else 'Fast check found misalignment :-(')
         return is_aligned
 
     def check_alignment(self):
-        t = self.Run.log_info('Checking aligment ... ', next_line=False)
+        t = self.Run.info('Checking aligment ... ', next_line=False)
         correlation = Correlation(self)
         for ev, row in self.TelRow.iteritems():
             correlation.fill(ev)
@@ -131,25 +71,25 @@ class PixAlignment:
             h.Fill(cor)
         set_root_output(0)
         fit = h.Fit('gaus', 'qs', '', .6, 1)
-        self.Run.format_histo(h, x_tit='Correlation Factor', y_tit='Number of Entries', y_off=1.4, stats=0)
-        self.Run.save_histo(h, 'EventAlignmentControl', show=False, lm=.13, prnt=False)
+        # self.Run.format_histo(h, x_tit='Correlation Factor', y_tit='Number of Entries', y_off=1.4, stats=0)
+        # self.Run.save_histo(h, 'EventAlignmentControl', show=False, lm=.13, prnt=False)
         mean_, sigma = fit.Parameter(1), fit.Parameter(2)
         if mean_ - 5 * sigma < .2:
-            self.Run.add_info(t)
-            log_message('run is very badly misaligned...')
+            self.Run.add_to_info(t)
+            info('run is very badly misaligned...')
             return False
         low_events = [cor for cor in correlations if cor < mean_ - 5 * sigma]
         misalignments = len(low_events) / float(len(correlations))
-        self.Run.add_info(t)
+        self.Run.add_to_info(t)
         if misalignments > .02:
-            log_message('found {v:5.2f} % misalignet events'.format(v=misalignments * 100))
+            info('found {v:5.2f} % misalignet events'.format(v=misalignments * 100))
             return False
         low_events = [cor for cor in correlations if cor < .3]
         misalignments = len(low_events) / float(len(correlations))
         if misalignments > .05:
-            log_message('found {v:5.2f} % misalignet events'.format(v=misalignments * 100))
+            info('found {v:5.2f} % misalignet events'.format(v=misalignments * 100))
         else:
-            self.Run.log_info('Everything is nicely aligned =)')
+            self.Run.info('Everything is nicely aligned =)')
         return misalignments < .05
 
     def find_lose_corr_event(self, correlation, last_off_event, debug=False):
@@ -179,6 +119,18 @@ class PixAlignment:
         if debug:
             print 'anti correlation:', min_anti_corr
         return inter_correlations[min_anti_corr] if min_anti_corr < -self.Threshold else None
+
+    def find_start_offset(self):
+        correlation = Correlation(self, bucket_size=200)
+        correlation.fill_n(self.TelRow.keys()[:200])
+        correlations = correlation.get_all()
+        return max(correlations, key=correlations.get) if any(array(correlations.values()) > self.Threshold) else 0
+
+    def find_final_offset(self):
+        correlation = Correlation(self, bucket_size=400, n_offsets=200)
+        correlation.fill_n(self.TelRow.keys()[-400:])
+        correlations = correlation.get_all()
+        return max(correlations, key=correlations.get)
 
     def draw_sliding(self, correlation):
         correlations = correlation.get_all_sliding()
@@ -239,10 +191,10 @@ class PixAlignment:
     def find_rising_event(self, correlations, ind):
         return correlations.keys()[correlations.values().index(next(c for c in correlations.values()[ind:] if c > self.Threshold)) - 3]
 
-    def find_offsets(self, debug=False):
-        t = self.Run.log_info('Scanning for precise offsets ... ', next_line=False)
+    def find_manual_offsets(self, debug=False):
+        t = self.Run.info('Scanning for precise offsets ... ', next_line=False)
 
-        n = self.BucketSize
+        n = self.BinSize
         correlation = Correlation(self, n_offsets=2, bucket_size=n)
 
         offsets = OrderedDict()
@@ -261,7 +213,7 @@ class PixAlignment:
                             offsets[l_off_event] = off
                             offsets[o_off_event] = -off
                             if debug:
-                                log_message('Found a jump of {v} between events {e1} and {e2}'.format(v=off, e1=l_off_event, e2=o_off_event))
+                                info('Found a jump of {v} between events {e1} and {e2}'.format(v=off, e1=l_off_event, e2=o_off_event))
                             # only keep the last two buckets since at least the last is correlated
                             correlation.reset_except_last(2)
                     # now we have lost correlation for at least three buckets
@@ -273,15 +225,14 @@ class PixAlignment:
                             offset += off
                             offsets[off_event] = off
                             if debug:
-                                log_message('Found an offset of {v} at event {e}'.format(v=off, e=off_event))
+                                info('Found an offset of {v} at event {e}'.format(v=off, e=off_event))
                             # delete the first two buckets and use the offset values for zero correlation
                             correlation.reshuffle(off)
                     if off is None and debug:
-                        log_message('Could not find any offset!')
+                        info('Could not find any offset!')
                 # reset old lists for speed improvements
                 correlation.reset()
-        self.Run.add_info(t)
-        log_message('Found {n} offsets'.format(n=len(offsets)))
+        self.Run.add_to_info(t)
         return offsets
 
     def find_bucket_size(self, show=True):
@@ -303,74 +254,35 @@ class PixAlignment:
                     corrs = correlation.get_all_zero()
                     mean_, sigma = calc_mean(corrs)
                     sigmas[sigma] = n
-                if show:
-                    g = self.Run.make_tgrapherrors('g_bs', 'Sigma of the Bucket Sizes', x=sigmas.values(), y=sigmas.keys())
-                    self.Run.draw_histo(g, draw_opt='alp')
+                # if show:
+                #     g = self.Run.make_tgrapherrors('g_bs', 'Sigma of the Bucket Sizes', x=sigmas.values(), y=sigmas.keys())
+                #     self.Run.draw_histo(g, draw_opt='alp')
                 size = next(n for sig, n in sigmas.iteritems() if sig < .09)
                 break
             except StopIteration:
                 correlation.delete_events(len(correlation.TelRow[0]) / 2, max_ev)
         return round_up_to(size, 5)
 
-    # =======================================================
-    # region WRITE TREE
-    def get_next_event(self):
-        if self.AtEntry == self.NEntries:
-            return False
-        self.InTree.GetEntry(self.AtEntry)
-        self.AtEntry += 1
-        return True
+    def fill_branches(self, offset):
+        if not offset:
+            event = self.Variables[self.AtEntry]
+        else:
+            tel_offset = abs(offset) if offset < 0 else 0
+            dut_offset = offset if offset > 0 else 0
+            # select the according events
+            tel_event, dut_event = self.Variables[self.AtEntry + tel_offset], self.Variables[self.AtEntry + dut_offset]
+            # merge the right parts of the events
+            event = concatenate((tel_event[:, tel_event[0] < self.NDutPlanes], dut_event[:, dut_event[0] >= self.NDutPlanes]), axis=1)
+        for i, name in enumerate(self.Branches.iterkeys()):
+            for value in event[i]:
+                self.Branches[name].push_back(value)
 
-    def set_branch_addresses(self):
-        for name, branch in self.Branches.iteritems():
-            self.NewTree.SetBranchAddress(name, branch)
 
-    def save_tree(self):
-        self.NewFile.cd()
-        self.NewTree.Write()
-        self.NewFile.Write()
+if __name__ == '__main__':
 
-    def clear_vectors(self):
-        for vec in self.Branches.itervalues():
-            vec.clear()
+    from pixel_run import PixelRun
+    from converter import Converter
 
-    def write_aligned_tree(self):
-        offsets = self.find_offsets()
-        self.NewFile = TFile(self.Converter.get_root_file_path(), 'RECREATE')
-        self.NewTree = self.InTree.CloneTree(0)
-        self.set_branch_addresses()
-        self.start_pbar(self.NEntries)
-        offset = 0
-        while self.get_next_event():
-            entry = self.AtEntry - 1
-            self.ProgressBar.update(self.AtEntry)
-            self.clear_vectors()
-            if entry in offsets:
-                offset += offsets[entry]
-            if not offset:
-                for name, lst in self.BranchLists.iteritems():
-                    for value in lst[entry]:
-                        self.Branches[name].push_back(value)
-            else:
-                ref = abs(offset) if offset < 0 else 0
-                dut = offset if offset > 0 else 0
-                # if we get out of range stop the loop
-                try:
-                    ref_plane = filter(lambda x: x < self.NDutPlanes, self.BranchLists['plane'][entry + ref])
-                    dut_plane = filter(lambda x: x >= self.NDutPlanes, self.BranchLists['plane'][entry + dut])
-                except IndexError:
-                    break
-                for i in xrange(len(ref_plane)):
-                    for name in self.Branches.iterkeys():
-                        self.Branches[name].push_back(self.BranchLists[name][entry + ref][i])
-                for i in xrange(len(dut_plane)):
-                    for name in self.Branches.iterkeys():
-                        self.Branches[name].push_back(self.BranchLists[name][entry + dut][-len(dut_plane) + i])
-            self.NewTree.Fill()
-        self.ProgressBar.finish()
-        self.save_tree()
-    # endregion
-
-    def start_pbar(self, n):
-        self.ProgressBar = ProgressBar(widgets=self.Widgets, maxval=n)
-        self.ProgressBar.start()
+    args = init_argparser(run=147, tc='201810')
+    zrun = PixelRun(args.run, test_campaign=args.testcampaign, tree=False, verbose=True)
+    z = PixAlignment(Converter(zrun))
