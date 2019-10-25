fc616c039d1c64488cb6b61779f3b1cb4fdd20d8
michareichmann <micha.reichmann@gmail.com>
2019-10-25 17:20:43 +0200

    move get methods and revise get_pulser_bins

--- src/plots.py
+++ src/plots.py
@@ -1,9 +1,9 @@
 from math import ceil, sqrt
 
 from ROOT import gStyle
-from numpy import arange, array, delete, append
+from numpy import arange, array, delete, append, concatenate
 
-from utils import increased_range
+from utils import increased_range, get_root_vec
 
 
 class Plots:
@@ -84,35 +84,6 @@ class Plots:
             ret_val.append(arr)
         return ret_val
 
-    def get_time_bins(self, evts_per_bin=None):
-        self.set_bin_size(evts_per_bin)
-        return [self.NBins, self.TimeBinning]
-
-    def get_event_bins(self, binning=None):
-        self.set_bin_size(binning)
-        return [self.NBins, array(self.Binning, 'd')]
-
-    def get_ph_bins(self, bin_width=1):
-        return [int(ceil((self.Settings['maxPadPh'] - self.Settings['minPadPh']) / float(bin_width))), self.Settings['minPadPh'], self.Settings['maxPadPh']]
-
-    def get_tcal_bins(self):
-        return [int(round(sqrt(len(self.Run.TCal))))] + increased_range([min(self.Run.TCal), max(self.Run.TCal)], .1, .1)
-
-    def get_global_bins(self, res, mode=None, arrays=False, mm=False):
-        x, y = array(self.Settings['globalCoods'][:2]), array(self.Settings['globalCoods'][2:])
-        size_x = self.Settings['xpix'] if mode in ['x', None] else self.Settings['ypix']
-        size_y = self.Settings['ypix'] if mode in ['y', None] else self.Settings['xpix']
-        x, y, size_x, size_y = [val * (10 if mm else 1) for val in [x, y, size_x, size_y]]
-        bins = [int(ceil((x[1] - x[0]) / size_x * sqrt(12) / res)), x[0], x[1], int(ceil((y[1] - y[0]) / size_y * sqrt(12) / res)), y[0], y[1]]
-        x_arr, y_arr = arange(x[0], x[1], size_x, 'd'), arange(y[0], y[1], size_y, 'd')
-        return bins if not arrays else [len(x_arr) - 1, x_arr, len(y_arr) - 1, y_arr]
-
-    def get_pulser_bins(self, n_pulser):
-        n = self.Run.tree.Draw('Entry$', 'pulser', 'goff')
-        pulser_events = [self.Run.tree.GetV1()[i] for i in xrange(n)]
-        binning = [pulser_events[i * n_pulser] for i in xrange(n / n_pulser)] + ([pulser_events[-1]] if n % n_pulser else [])
-        return len(binning) - 1, array(binning)
-
     # ----------------------------------------
     # region BINNING
     def get_raw_binning(self, evts_per_bin, start_event=0, end_event=None, time_bins=False):
@@ -132,7 +103,8 @@ class Plots:
         if self.Cut is None:
             return self.get_raw_binning(evts_per_bin)
         jumps = filter(lambda x: x[1] > self.Cut.get_min_event(), self.Cut.Interruptions)  # filter out interruptions outside event range
-        events = arange(min(self.Cut.get_min_event(), jumps[0][1]), self.Cut.get_max_event())
+        first_jump_end = jumps[0][1] if jumps else 0
+        events = arange(min(self.Cut.get_min_event(), first_jump_end), self.Cut.get_max_event())
         for start, stop in jumps:  # remove beam interruptions from events
             events = delete(events, arange(start, stop + 1))
         events = events[::evts_per_bin]  # slice out every nth entry
@@ -152,5 +124,35 @@ class Plots:
         for event in self.Binning:
             time_bins.append(self.Run.get_time_at_event(event))
         return array(time_bins, 'd')
+
+    def get_time_bins(self, evts_per_bin=None):
+        self.set_bin_size(evts_per_bin)
+        return [self.NBins - 1, self.TimeBinning]
+
+    def get_event_bins(self, binning=None):
+        self.set_bin_size(binning)
+        return [self.NBins - 1, array(self.Binning, 'd')]
+
+    def get_ph_bins(self, bin_width=1):
+        return [int(ceil((self.Settings['maxPadPh'] - self.Settings['minPadPh']) / float(bin_width))), self.Settings['minPadPh'], self.Settings['maxPadPh']]
+
+    def get_tcal_bins(self):
+        return [int(round(sqrt(len(self.Run.TCal))))] + increased_range([min(self.Run.TCal), max(self.Run.TCal)], .1, .1)
+
+    def get_global_bins(self, res, mode=None, arrays=False, mm=False):
+        x, y = array(self.Settings['globalCoods'][:2]), array(self.Settings['globalCoods'][2:])
+        size_x = self.Settings['xpix'] if mode in ['x', None] else self.Settings['ypix']
+        size_y = self.Settings['ypix'] if mode in ['y', None] else self.Settings['xpix']
+        x, y, size_x, size_y = [val * (10 if mm else 1) for val in [x, y, size_x, size_y]]
+        bins = [int(ceil((x[1] - x[0]) / size_x * sqrt(12) / res)), x[0], x[1], int(ceil((y[1] - y[0]) / size_y * sqrt(12) / res)), y[0], y[1]]
+        x_arr, y_arr = arange(x[0], x[1], size_x, 'd'), arange(y[0], y[1], size_y, 'd')
+        return bins if not arrays else [len(x_arr) - 1, x_arr, len(y_arr) - 1, y_arr]
+
+    def get_pulser_bins(self, n_pulser):
+        n = self.Run.Tree.Draw('Entry$', 'pulser', 'goff')
+        pulser_events = get_root_vec(self.Run.Tree, n)
+        bins = concatenate([[0], pulser_events[arange(n_pulser, n, n_pulser)], [pulser_events[-1]]])
+        bins = bins[:-1] if bins[-1] == bins[-2] else bins
+        return bins.size - 1, bins
     # endregion BINNING
     # ----------------------------------------
