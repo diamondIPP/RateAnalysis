e5fff9cde4037f7ec664e9efbba9c1f1f27c2f55
michareichmann <micha.reichmann@gmail.com>
2019-10-31 13:28:04 +0100

    add collection

--- analyse.py
+++ analyse.py
@@ -10,15 +10,18 @@ path.append(join(dirname(realpath(__file__)), 'src'))
 from utils import *
 from run import Run
 
-args = init_argparser(run=23, tc='201908', dia=1, tree=True, verbose=True, collection=True)
+args = init_argparser(run=139, tc='201810', dia=1, tree=True, verbose=True, collection=True)
 
 if not args.collection:
     from pad_analysis import PadAnalysis
-    from PixAnalysis import PixAnalysis
-    dummy = Run(args.run, args.testcampaign, tree=False)
+    from pix_analysis import PixAnalysis
+    dummy = Run(args.runplan, args.testcampaign, tree=False)
     if dummy.get_type() == 'pad':
-        z = PadAnalysis(args.run, args.dia, args.testcampaign, args.tree, args.verbose)
+        z = PadAnalysis(args.runplan, args.dia, args.testcampaign, args.tree, None, args.verbose)
     elif dummy.get_type() == 'pixel':
-        z = PixAnalysis(args.run, args.dia, args.testcampaign, args.tree, args.verbose)
+        z = PixAnalysis(args.runplan, args.dia, args.testcampaign, args.tree, None, args.verbose)
     else:
         critical('wrong run type: has to be in [pad, pixel]')
+else:
+    from analysis_collection import AnalysisCollection
+    z = AnalysisCollection(args.runplan, args.dia, args.testcampaign, args.tree, args.verbose)
