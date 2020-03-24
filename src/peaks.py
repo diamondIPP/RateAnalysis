70e6041f76b004132b4776d371b83ece6073a466
michareichmann <micha.reichmann@gmail.com>
2020-03-24 12:21:04 +0100

    add methods to find peaks in waveform

--- src/peaks.py
+++ src/peaks.py
@@ -4,10 +4,11 @@
 # created on June 7th 2017 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
 
-from analysis import Analysis, format_histo
+from analysis import *
 from ROOT import TH1F, TCut, gROOT
-from utils import fit_poissoni, warning
-from math import pi
+from scipy.signal import find_peaks, savgol_filter
+from numpy import polyfit, pi, RankWarning
+from warnings import simplefilter
 
 
 class PeakAnalysis(Analysis):
@@ -17,32 +18,66 @@ class PeakAnalysis(Analysis):
         Analysis.__init__(self, verbose=self.Ana.Verbose)
         self.Run = self.Ana.Run
         self.Channel = self.Ana.Channel
-        self.DUT = self.Ana.DUT.Number
+        self.DUT = self.Ana.DUT
         self.Tree = self.Ana.Tree
-        self.set_save_directory(self.Ana.SubDir)
+        self.NoiseThreshold = abs(self.Ana.Pedestal.get_mean() + 5 * self.Ana.Pedestal.get_noise())  # threshold of the peaks = five times above the noise
 
-    def draw_n_peaks(self, spec=False, show=True, fit=False):
+    def find_all(self):
+        events = self.Run.get_root_vec(var='Entry$', cut=self.Ana.Cut(), dtype=int)
+        peak_times = []
+        simplefilter('ignore', RankWarning)
+        self.Ana.PBar.start(events.size)
+        for event in events:
+            peak_times.append(self.find(event))
+            self.Ana.PBar.update()
+        return peak_times
+
+    def find(self, event=1819):
+        self.Tree.GetBranch('wf0').GetEntry(event)
+        self.Tree.GetBranch('trigger_cell').GetEntry(event)
+        values = self.Ana.Polarity * array(getattr(self.Tree, 'wf{}'.format(self.Channel)))
+        x = array(self.Ana.Waveform.get_calibrated_times(self.Tree.trigger_cell))
+        peak_values = find_peaks(values, height=self.NoiseThreshold, distance=2, prominence=20)[0]
+        peak_values = peak_values[(peak_values > 10) & (peak_values < 1014)]
+        time_peaks = []
+        # g = self.make_tgrapherrors('gp', 'Signal Peaks', x=x, y=values)
+        # self.draw_histo(g)
+        for peak in peak_values:
+            x1 = x[peak - 10:peak + 12]
+            y = savgol_filter(values[peak - 10:peak + 12], 15, 3)
+            p_max = where(y == y.max())[0][0]
+            if 5 > p_max < 15:
+                continue
+            p2, p1, p0 = polyfit(x1[p_max - 1:p_max + 2], y[p_max - 1:p_max + 2], deg=2)
+            # f = TF1('f', 'pol2', x1[0], x1[-1])
+            # f.SetParameters(p0, p1, p2)
+            # f.SetNpx(1000)
+            # self.draw_histo(f, draw_opt='same', canvas=get_last_canvas())
+            time_peaks.append(-p1/2/p2)
+        return time_peaks
+
+    def draw_n_peaks(self, spec=False, show=True, do_fit=False):
         h = TH1F('h_pn', 'Number of Peaks', 10, 0, 10)
         draw_var = '@peaks{ch}_x.size()>>h_pn' if spec and self.Run.has_branch('peaks{ch}_x'.format(ch=self.Channel)) else 'n_peaks[{ch}] - 1>>h_pn'
         self.Tree.Draw(draw_var.format(ch=self.Channel), self.Ana.Cut(), 'goff')
-        self.format_statbox(only_fit=True, w=.3) if fit else self.format_statbox(entries=True)
+        self.format_statbox(only_fit=True, w=.3) if do_fit else self.format_statbox(entries=True)
         format_histo(h, x_tit='Number of Peaks', y_tit='Number of Entries', y_off=1.4, fill_color=self.FillColor, lw=2)
         self.save_histo(h, 'PeakNumbers', show, logy=True, lm=.11)
         return h
 
     def draw_n_peaks_fit(self, show=True, spec=False):
-        h = self.draw_n_peaks(spec, show=False, fit=True)
+        h = self.draw_n_peaks(spec, show=False, do_fit=True)
         format_histo(h, 'Fit Result')
         self.draw_histo(h, '', show, logy=True, lm=.11)
-        fit = fit_poissoni(h, show=show)
+        fit_func = fit_poissoni(h, show=show)
         self.save_histo(h, 'PeakNumbersFit', show, logy=True, lm=.11, draw_opt='e1same', canvas=gROOT.GetListOfCanvases()[-1])
-        self.get_flux(fit=fit)
-        return fit
+        self.get_flux(fit_func=fit_func)
+        return fit_func
 
-    def get_flux(self, fit=None):
-        fit = self.draw_n_peaks_fit(show=False) if fit is None else fit
-        lambda_ = fit.GetParameter(1)
-        err = fit.GetParError(1)
+    def get_flux(self, fit_func=None):
+        fit_func = self.draw_n_peaks_fit(show=False) if fit_func is None else fit_func
+        lambda_ = fit_func.GetParameter(1)
+        err = fit_func.GetParError(1)
         n_bunches = 25
         proc_frequency = 50.6e6
         flux = lambda_ * proc_frequency / (n_bunches * self.get_area()) / 1000.
