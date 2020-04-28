73ce697b426b6455b0ded4712b7cad168413d8a9
michareichmann <micha.reichmann@gmail.com>
2020-04-28 10:41:42 +0200

    merge all dia info into one file stored at the data directory

--- src/dut.py
+++ src/dut.py
@@ -2,7 +2,7 @@
 #       cut sub class to handle all the cut strings for the DUTs with digitiser
 # created in 2015 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
-from analysis import load_parser, get_base_dir, OrderedDict, critical, Analysis, join
+from analysis import load_json, get_base_dir, OrderedDict, critical, Analysis, join
 from json import load, loads
 
 
@@ -20,11 +20,13 @@ class DUT:
         self.Attenuator = run_info['att_dia{}'.format(self.Number)] if 'att_dia{}'.format(self.Number) in run_info else None
 
         # Specs
-        self.Specs = load_parser(join(self.Config.get('MAIN', 'data directory'), 'diaSpecs.ini'))
-        self.Irradiation = self.load_irradiation()
-        self.Thickness = self.load_spec('Thickness', typ=int, default=500)
+        self.Specs = load_json(join(self.Config.get('MAIN', 'data directory'), 'dia_info.json'))[self.Name]
+        self.Irradiation = self.load_spec('irradiation')
+        self.Thickness = self.load_spec('thickness', typ=int, default=500)
         self.CCD = self.load_spec('CCD', typ=int)
-        self.Size = loads(self.load_spec('Size')) if self.load_spec('Size') is not None else None
+        self.Size = self.load_spec('size', lst=True)
+        self.Metal = self.load_spec('metal', typ=float)
+        self.GuardRing = self.load_spec('guard ring', typ=float)
 
     def __str__(self):
         return 'DUT {}, {}, Bias: {:1.0f}V'.format(self.Number, self.Name, self.Bias)
@@ -40,8 +42,9 @@ class DUT:
     def get_irradiation(self, tc):
         return self.Irradiation[tc] if tc in self.Irradiation else critical('Please add "{}" to the irradiation file for {}'.format(self.Name, tc))
 
-    def load_spec(self, section, typ=None, default=None):
-        return default if not self.Specs.has_option(section, self.Name) else self.Specs.get(section, self.Name) if typ is None else typ(self.Specs.get(section, self.Name))
+    def load_spec(self, section, typ=None, lst=False, default=None):
+        spec = default if section not in self.Specs or self.Specs[section] == 'None' else self.Specs[section] if typ is None else typ(self.Specs[section])
+        return loads(spec) if lst and spec is not None else spec
 
     def set_number(self, value):
         self.Number = value
