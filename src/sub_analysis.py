1133167448c0077d74912de60183c93785dbc644
michareichmann <micha.reichmann@gmail.com>
2020-10-05 14:43:51 +0200

    update sub_ana methods

--- src/sub_analysis.py
+++ src/sub_analysis.py
@@ -10,13 +10,23 @@ class SubAnanlysis(Analysis):
     """ small module to create all required fields for the subanalyses. """
 
     def __init__(self, analysis, results_dir='', pickle_dir=''):
-        super().__init__(analysis.TCString, results_dir, pickle_dir)
 
         self.Ana = analysis
         self.Run = analysis.Run
         self.Tree = analysis.Tree
         self.Bins = analysis.Bins
         self.Cut = analysis.Cut
+        self.NRocs = analysis.NRocs
+        super().__init__(analysis.TCString, results_dir, pickle_dir)
 
     def get_root_vec(self, n=0, ind=0, dtype=None, var=None, cut=''):
         return self.Run.get_root_vec(n, ind, dtype, var, cut)
+
+    def has_branch(self, branch):
+        return self.Run.has_branch(branch)
+
+    def get_t_var(self):
+        return self.Ana.get_t_var()
+
+    def get_event_at_time(self, seconds, rel=False):
+        return self.Run.get_event_at_time(seconds, rel)
