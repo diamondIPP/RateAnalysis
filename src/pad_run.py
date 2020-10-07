2c20af178694e910b9a99ccdb706667fb9b911c4
michareichmann <micha.reichmann@gmail.com>
2020-10-07 16:11:12 +0200

    futurise pad_run and add from_parent()

--- src/pad_run.py
+++ src/pad_run.py
@@ -3,31 +3,27 @@
 #       pad run class
 # created on Oct 15th 2019 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
-
-from ConfigParser import ConfigParser
-from StringIO import StringIO
 from collections import OrderedDict
-from json import dumps, loads
-
+from configparser import ConfigParser
+from json import loads
 from numpy import array, concatenate, cumsum
-
+from helpers.utils import has_bit, critical, warning, ensure_dir, init_argparser
 from run import Run, join
-from utils import has_bit, critical, warning, ensure_dir, init_argparser
 
 
 class PadRun(Run):
     """ Run class containing all the information for a single run. """
 
-    def __init__(self, number=None, test_campaign=None, tree=True, t_vec=None, verbose=False):
+    def __init__(self, number=None, testcampaign=None, tree=True, t_vec=None, verbose=False):
         """
         :param number: if None is provided it creates a dummy run
-        :param test_campaign: if None is provided ...
+        :param testcampaign: if None is provided ...
         :param tree: root_tree object, if None is given it will start the converter
         :param t_vec: time sequence of the run, if None is provide it will generate a corrected one
         :param verbose: turn on more output
         """
 
-        Run.__init__(self, number, test_campaign, tree, t_vec, verbose)
+        Run.__init__(self, number, testcampaign, tree, t_vec, verbose)
 
         # Settings
         self.NChannels = self.load_pre_n_channels()
@@ -46,6 +42,10 @@ class PadRun(Run):
             self.NSamples = len(self.TCal)
             self.Channels = self.load_channels()
 
+    @classmethod
+    def from_parent(cls, parent: Run):
+        return cls(parent.Number, parent.TCString, tree=(parent.RootFile, parent.Tree), t_vec=parent.Time)
+
     def load_rootfile_dirname(self):
         return ensure_dir(join(self.TCDir, 'root', 'pads'))
 
@@ -57,33 +57,15 @@ class PadRun(Run):
             return [i for i, val in enumerate(self.TreeConfig.get('General', 'active regions').split()) if int(val)]
         # read it from the current config file if there is no tree loaded
         binary = self.Config.getint('ROOTFILE_GENERATION', 'active_regions')
-        return [i for i in xrange(self.NChannels) if has_bit(binary, i)]
+        return [i for i in range(self.NChannels) if has_bit(binary, i)]
 
     def load_tree_config(self):
         macro = self.RootFile.Get('region_information')
-        info = '\n'.join(str(word) for word in macro.GetListOfLines())
+        info = [str(line).strip('\n') for line in macro.GetListOfLines()]
+        if not info[0].startswith('['):
+            critical('very old data! rerun conversion!')
         config = ConfigParser()
-        if not info.startswith('['):
-            info = []
-            for word in macro.GetListOfLines():
-                word = str(word).strip('\n *:')
-                if word.startswith('active'):
-                    info.append('[General]')
-                    data = word.replace('_', ' ').split(':')
-                    word = '{0} = {1}'.format(data[0], ' '.join(bin(int(data[1]))[2:]))
-                elif word.startswith('Signal') or word.startswith('Sensor'):
-                    word = '[{}]'.format(word)
-                elif word.startswith('tcal'):
-                    info.append('[Time Calibration]')
-                    word = word.replace('tcal', 'tcal =').replace(', \b\b', '')
-                elif word and word[-1].isdigit() and 'pulser,' not in word.lower():
-                    data = word.split(':')
-                    word = '{0} = {1}'.format(data[0], str([int(num) for num in data[1].split('-')]))
-                elif 'pulser' in word.lower():
-                    word = 'Names = {}'.format(dumps(word.split(',')))
-                info.append(word)
-            info = '\n'.join(info)
-        config.readfp(StringIO(info))
+        config.read_file(info)
         return config
 
     def load_digitizer_channels(self):
@@ -114,7 +96,7 @@ class PadRun(Run):
 
     def get_calibrated_time(self, trigger_cell, ibin):
         v = self.TCal[int(trigger_cell)]
-        for i in xrange(ibin):
+        for i in range(ibin):
             v += self.TCal[(int(trigger_cell) + i + 1) % self.NSamples]
         return v
 
@@ -133,9 +115,10 @@ class PadRun(Run):
         macro = self.RootFile.Get('region_information')
         for line in macro.GetListOfLines():
             line = str(line)
-            print 'tcal = {}'.format(array(loads(line.strip('tcal =')))) if line.startswith('tcal') else line
+            print('tcal = {}'.format(array(loads(line.strip('tcal =')))) if line.startswith('tcal') else line)
 
 
 if __name__ == '__main__':
-    args = init_argparser(run=23, tc='201908', tree=True)
-    z = PadRun(args.run, tree=args.tree, test_campaign=args.testcampaign)
+    args = init_argparser(run=88, tc='201908', tree=True)
+    r = Run(args.run, tree=args.tree, testcampaign=args.testcampaign, verbose=1)
+    z = PadRun.from_parent(r)
