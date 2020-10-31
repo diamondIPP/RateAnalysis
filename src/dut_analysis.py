214e9461f038ffde7559bdda412be077efb0c4c9
michareichmann <micha.reichmann@gmail.com>
2020-10-31 13:50:26 +0100

    small fix

--- src/dut_analysis.py
+++ src/dut_analysis.py
@@ -4,7 +4,7 @@
 # created on Oct 30th 2019 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
 
-from numpy import vectorize, meshgrid, diff, digitize, cumsum, histogram2d, split, lexsort
+from numpy import vectorize, meshgrid, diff, digitize, histogram2d, lexsort
 from numpy.random import rand
 from uncertainties import umath
 
@@ -76,7 +76,7 @@ class DUTAnalysis(Analysis):
         return self.Run.StartTime if rel_time else 0
 
     def get_t_args(self, rel_time):
-        return {'x_tit': 'Time [mm::hh]', 't_ax_off': self.get_t_off(rel_time)}
+        return {'x_tit': 'Time [hh::mm]', 't_ax_off': self.get_t_off(rel_time)}
 
     def get_root_vec(self, n=0, ind=0, dtype=None, var=None, cut='', nentries=None, firstentry=0):
         return self.Run.get_root_vec(n, ind, dtype, var, cut, nentries, firstentry)
