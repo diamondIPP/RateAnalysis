6f65b860a8842a2a7b15b75d3a12e9370325767d
michareichmann <micha.reichmann@gmail.com>
2020-11-09 10:02:56 +0100

    fix statbox

--- helpers/draw.py
+++ helpers/draw.py
@@ -564,7 +564,7 @@ def format_histo(histo, name=None, title=None, x_tit=None, y_tit=None, z_tit=Non
     return h
 
 
-def format_statbox(x=.95, y=None, w=.4, h=.15, only_fit=False, fit=False, entries=False, form=None, m=False, rms=False, all_stat=False, center_x=False):
+def format_statbox(x=.95, y=None, w=.2, h=.15, only_fit=False, fit=False, entries=False, form=None, m=False, rms=False, all_stat=False, center_x=False):
     gStyle.SetOptFit(int(only_fit or fit))
     opt_stat = '100000{}{}{}0'.format(*ones(3, 'i') if all_stat else array([rms, m, entries]).astype('i'))
     if only_fit:
@@ -574,7 +574,7 @@ def format_statbox(x=.95, y=None, w=.4, h=.15, only_fit=False, fit=False, entrie
     gStyle.SetFitFormat(form) if form is not None else do_nothing()
     gStyle.SetStatX(.5 + w / 2 if center_x else x)
     gStyle.SetStatY(y)
-    gStyle.SetStatW(w - .2)
+    gStyle.SetStatW(w - (.2 if only_fit or fit else 0))
     gStyle.SetStatH(h)
 
 
