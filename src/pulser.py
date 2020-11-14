9368d87eebf68aff4b9d3335bee99a2d3fbb07c6
michareichmann <micha.reichmann@gmail.com>
2020-11-14 17:07:55 +0100

    add get_sigma()

--- src/pulser.py
+++ src/pulser.py
@@ -59,10 +59,13 @@ class PulserAnalysis(PadSubAnalysis):
     def get_t_bins(self, bin_size=None):
         return Bins.make(*self.SignalRegion, choose(bin_size, default=self.Waveform.BinWidth))
 
-    def get_pulse_height(self, corr=True, bin_width=.2, redo=False):
+    def get_pulse_height(self, corr=True, bin_width=.2, par=1, redo=False):
         pickle_path = self.make_simple_pickle_path('HistoFit', '{}_{}'.format(int(corr), 'Beam1'))
         fit = do_pickle(pickle_path, partial(self.draw_distribution_fit, show=False, prnt=False, corr=corr, redo=redo, bin_width=bin_width), redo=redo)
-        return make_ufloat(fit, par=1)
+        return make_ufloat(fit, par=par)
+
+    def get_sigma(self, corr=True, bin_width=.2, redo=False):
+        return self.get_pulse_height(corr, bin_width, par=2, redo=redo)
 
     def get_pedestal(self, par=1, redo=False):
         pickle_path = self.make_simple_pickle_path('Pedestal')
