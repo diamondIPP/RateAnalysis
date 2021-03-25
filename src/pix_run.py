bad4be208c7a0b2dd940bd7c46ff8b0d86c4eb34
michareichmann <micha.reichmann@gmail.com>
2021-03-25 08:28:35 +0100

    py fix for pix run

--- src/pix_run.py
+++ src/pix_run.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # --------------------------------------------------------
-#       pad run class
+#       pixel run class
 # created on Oct 21st 2019 by M. Reichmann (remichae@phys.ethz.ch)
 # --------------------------------------------------------
 
-from run import Run, join
-from utils import ensure_dir, init_argparser
+from run import Run
+from helpers.utils import init_argparser
 
 
 class PixelRun(Run):
     """ Run class containing all the information for a single run. """
 
-    def __init__(self, number=None, test_campaign=None, tree=True, t_vec=None, verbose=False):
+    def __init__(self, number=None, testcampaign=None, tree=True, t_vec=None, verbose=False):
         """
         :param number: if None is provided it creates a dummy run
-        :param test_campaign: if None is provided ...
+        :param testcampaign: if None is provided ...
         :param tree: root_tree object, if None is given it will start the converter
         :param t_vec: time sequence of the run, if None is provide it will generate a corrected one
         :param verbose: turn on more output
@@ -23,13 +23,10 @@ class PixelRun(Run):
         # Settings
         self.Type = 'pixel'
 
-        Run.__init__(self, number, test_campaign, tree, t_vec, verbose)
-
-    def load_rootfile_dirname(self):
-        return ensure_dir(join(self.TCDir, 'root', self.Type))
+        Run.__init__(self, number, testcampaign, tree, t_vec, verbose)
 
 
 if __name__ == '__main__':
 
-    args = init_argparser(23, tc='201809', tree=True)
-    z = PixelRun(args.run, tree=args.tree, test_campaign=args.testcampaign)
+    args = init_argparser(489, tc='201610', tree=True)
+    z = PixelRun(args.run, tree=args.tree, testcampaign=args.testcampaign)
