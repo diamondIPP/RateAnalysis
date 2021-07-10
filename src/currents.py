99bfffd1f5de69aa3588db220d6f812ed8143597
michareichmann <micha.reichmann@gmail.com>
2021-07-10 20:32:35 +0200

    small fix

--- src/currents.py
+++ src/currents.py
@@ -1,7 +1,7 @@
 from glob import glob
 from os.path import getsize
 from ROOT import TH2F
-from numpy import genfromtxt, isnan, datetime64, invert, uint32, char, inf
+from numpy import genfromtxt, isnan, datetime64, invert, uint32, char
 from helpers.save_plots import *
 from src.analysis import Analysis
 from src.run_selection import RunSelector
