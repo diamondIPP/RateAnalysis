f920c10f17ef307057064f132f2188cf803ffb8d
michareichmann <micha.reichmann@gmail.com>
2021-08-16 18:13:44 +0200

    revise say such that it's usable via ssh

--- helpers/utils.py
+++ helpers/utils.py
@@ -18,7 +18,7 @@ from time import time, sleep
 from gtts import gTTS
 from numpy import sqrt, array, average, mean, arange, log10, concatenate, where, count_nonzero, full, ndarray, exp, sin, cos, arctan, zeros, dot, roll, arctan2, frombuffer, split, cumsum
 from numpy import histogram, log2, diff, isfinite, pi, corrcoef, quantile
-from os import makedirs, _exit, remove, devnull, stat
+from os import makedirs, _exit, remove, devnull, stat, getenv
 from os import path as pth
 from os.path import dirname, realpath, join
 from pytz import timezone, utc
@@ -541,7 +541,7 @@ def say(txt, lang='en'):
     tts = gTTS(text=txt, lang=lang)
     tts.save('good.mp3')
     with open(devnull, 'w') as FNULL:
-        call(['mpg321', 'good.mp3'], stdout=FNULL)
+        call(([] if getenv('SSH_TTY') is None else ['DISPLAY=:0']) + ['mpg321', 'good.mp3'], stdout=FNULL)
     remove('good.mp3')
 
 
