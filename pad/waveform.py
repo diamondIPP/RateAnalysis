3ea8a3d32b4b2c4794984140a23bc1a3b23e17d0
michareichmann <micha.reichmann@gmail.com>
2021-09-07 17:25:00 +0200

    save waveforms plot

--- pad/waveform.py
+++ pad/waveform.py
@@ -143,11 +143,11 @@ class Waveform(PadSubAnalysis):
         self.Draw(h, 'WaveForms{n}'.format(n=n), show, draw_opt='col' if n > 1 else 'apl', lm=.073, rm=.045, bm=.18, w=1.5, h=.5, gridy=grid, gridx=grid)
         return h, self.Count - start_count
 
-    def draw_all(self, signal_corr=False, raw=False, n=None, x_range=None, y_range=None, cut=None, channel=None, grid=True, **kwargs):
+    def draw_all(self, signal_corr=False, raw=False, n=None, x_range=None, y_range=None, cut=None, channel=None, **kwargs):
         n = choose(n, 100000)
         y, x, bins = self.get_values(cut, channel, n), self.get_times(signal_corr, cut, n, raw), self.get_binning() + make_bins(-512, 512.1, .5)
         rx, ry = choose(x_range, [0, 512]), choose(y_range, ax_range(min(y), max(y), .1, .2)),
-        h = self.Draw.histo_2d(x, y, bins, f'{n} Waveforms', **Draw.mode(3), **kwargs, x_range=rx, y_range=ry, stats=False, grid=grid, gridy=True, logz=True, draw_opt='col')
+        h = self.Draw.histo_2d(x, y, bins, f'{n} Waveforms', **Draw.mode(3), **kwargs, x_range=rx, y_range=ry, stats=False, grid=True, logz=True, draw_opt='col', file_name='WaveForms100k')
         return h, n
 
     def draw_single(self, cut=None, ind=None, x_range=None, y_range=None, t_corr=True, grid=True, raw=False, show=True, show_noise=False, draw_opt='ap'):
