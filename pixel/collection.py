2851ae25f577987d271adcc2aae050f2bcb719fb
michareichmann <micha.reichmann@gmail.com>
2022-01-06 15:53:01 +0100

    revise pix collection

--- pixel/collection.py
+++ pixel/collection.py
@@ -5,7 +5,7 @@
 # --------------------------------------------------------
 
 from src.analysis_collection import AnalysisCollection
-from pixel.analysis import PixAnalysis, init_argparser, array, prep_kw
+from pixel.analysis import PixAnalysis, init_argparser, array, prep_kw, ufloat, sqrt, quiet
 
 
 class PixCollection(AnalysisCollection):
@@ -13,19 +13,37 @@ class PixCollection(AnalysisCollection):
     def __init__(self, run_plan, dut_nr, test_campaign=None, load_tree=True, verbose=False):
         AnalysisCollection.__init__(self, run_plan, dut_nr, test_campaign, load_tree, verbose)
 
+        self.N = self.FirstAnalysis.N
+
     @staticmethod
     def load_dummy():
         return PixAnalysis
 
-    def get_hit_efficiecies(self):
-        return array(f[0] for f in self.get_values('hit eff', self.Analysis.draw_hit_efficiency, show=False))
+    @quiet
+    def save_coll_plots(self):
+        self.draw_efficiencies(show=False)
+        self.draw_cluster_sizes(show=False)
+        super(PixCollection, self).save_coll_plots()
 
-    def draw_hit_efficiencies(self, **kwargs):
-        x, y = self.get_fluxes(), self.get_hit_efficiecies()
-        self.Draw.graph(x, y, 'Hit Efficiencies', **prep_kw(kwargs, y_tit='Hit Efficiency [%]', **self.get_x_args(draw=True), draw_opt='alp'))
+    # ----------------------------------------
+    # region GET
+    def get_pulse_heights(self, bin_width=None, redo=False, runs=None, avrg=False, flux_sort=False, err=False, **kw):
+        picklepath = self.get_pickle_path('Fit', sub_dir='PH')
+        x = self.get_values('pulse heights', self.Analysis.get_pulse_height, runs, avrg=avrg, picklepath=picklepath, bin_size=bin_width, redo=redo, flux_sort=flux_sort)
+        return array([ufloat(ph.n, sqrt(ph.s ** 2 + ((self.get_sys_error() if err else 0) * ph.n) ** 2)) for ph in x])
 
-    def generate_threshold_pickle(self):
-        pass
+    def get_efficiencies(self, *args):
+        return super(PixCollection, self).get_efficiencies('0')
+    # endregion GET
+    # ----------------------------------------
+
+    def draw_efficiencies(self, avrg=False, t=False, **dkw):
+        x, y = self.get_x_var(t, avrg=avrg), self.get_efficiencies()
+        return self.Draw.graph(x, y, 'Hit Efficiencies', **prep_kw(dkw, y_tit='Hit Efficiency [%]', **self.get_x_args(t, draw=True), draw_opt='alp', file_name=f'Efficiencies{"Avr" if avrg else ""}'))
+
+    def draw_cluster_sizes(self, avrg=False, t=False, redo=False, **dkw):
+        x, y = self.get_x_var(t, avrg=avrg), self.get_values('cluster sizes', self.Analysis.get_cluster_size, None, 1, avrg, self.get_pickle_path('CS', '', 'Telescope', self.N), redo=redo)
+        return self.Draw.graph(x, y[:, 0], 'Cluster Size', **prep_kw(dkw, y_tit='Cluster Size', **self.get_x_args(t, draw=True), draw_opt='alp', file_name=f'ClusterSize{"Avr" if avrg else ""}'))
 
 
 if __name__ == '__main__':
